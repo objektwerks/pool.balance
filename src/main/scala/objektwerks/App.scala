d845eee3148f0a08b9b5a67219e745555b9d8667
objektwerks <objektwerks@runbox.com>
2022-05-24 20:13:14 -0400

    edite app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,5 +1,6 @@
 package objektwerks
 
+import scalafx.Includes._
 import scalafx.application.JFXApp3
 import scalafx.scene.Scene
 import scalafx.scene.paint.Color._
@@ -8,17 +9,12 @@ import scalafx.scene.shape.Rectangle
 object App extends JFXApp3 {
   override def start(): Unit = {
     stage = new JFXApp3.PrimaryStage {
-      title.value = "Hello Stage"e
+      title.value = "Pool Calc"
       width = 600
       height = 450
       scene = new Scene {
         fill = LightGreen
         content = new Rectangle {
-          x = 25
-          y = 40
-          width = 100
-          height = 100
-          fill <== when(hover) choose Green otherwise Red
         }
       }
     }
