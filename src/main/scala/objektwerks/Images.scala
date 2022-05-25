e0cd94e314e8b50624e8df3fa8080b95295e9359
objektwerks <objektwerks@runbox.com>
2022-05-25 08:21:12 -0400

    added new logo png files; edited images

--- src/main/scala/objektwerks/Images.scala
+++ src/main/scala/objektwerks/Images.scala
@@ -3,7 +3,7 @@ package objektwerks
 import scalafx.scene.image.{Image, ImageView}
 
 object Images:
-  val logo = new Image(Images.getClass.getResourceAsStream("/logo.png"))
+  val logo = new Image(Images.getClass.getResourceAsStream("/logo.white.png"))
 
   def loadImageView(path: String): ImageView = new ImageView {
     image = new Image(Images.getClass.getResourceAsStream(path))
