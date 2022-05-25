226106c5f06c45de43876233ec4ee203da4c068d
objektwerks <objektwerks@runbox.com>
2022-05-25 08:05:30 -0400

    enhanced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -8,12 +8,25 @@ import scalafx.scene.paint.Color._
 import scalafx.scene.shape.Rectangle
 import scalafx.Includes._
 
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.control.SplitPane
+import scalafx.scene.layout.{Priority, VBox}
+
 object App extends JFXApp3:
   val conf = ConfigFactory.load("app.conf")
 
   override def start(): Unit =
     stage = new JFXApp3.PrimaryStage {
-      val sceneGraph = new Scene {}
+      scene = new Scene {
+        root = new VBox {
+          prefHeight = 600
+          prefWidth = 800
+          spacing = 6
+          padding = Insets(6)
+          children = List()
+        }
+      }
       title = conf.getString("title")
       minHeight = conf.getInt("height").toDouble
       minWidth = conf.getInt("width").toDouble
