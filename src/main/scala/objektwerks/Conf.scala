04172aea38690e398e6ce83402dca88c38aca92b
objektwerks <objektwerks@runbox.com>
2022-05-26 14:23:26 -0400

    moved Image code into Conf

--- src/main/scala/objektwerks/Conf.scala
+++ src/main/scala/objektwerks/Conf.scala
@@ -1,9 +1,21 @@
 package objektwerks
 
 import com.typesafe.config.ConfigFactory
+import scalafx.scene.image.{Image, ImageView}
 
 object Conf:
   private val conf = ConfigFactory.load("app.conf")
+
   val windowTitle = conf.getString("window.title")
   val windowWidth = conf.getDouble("window.width")
   val windowHeight = conf.getDouble("window.height")
+
+  val logo = new Image(Image.getClass.getResourceAsStream("/logo.white.png"))
+
+  def loadImageView(path: String): ImageView = new ImageView {
+    image = new Image(Image.getClass.getResourceAsStream(path))
+    fitHeight = 25
+    fitWidth = 25
+    preserveRatio = true
+    smooth = true
+  }
