01a3af99ea24c6e57c2adc2f515ec25fe29e422c
objektwerks <objektwerks@runbox.com>
2022-05-26 14:28:04 -0400

    added View

---
+++ src/main/scala/objektwerks/View.scala
@@ -0,0 +1,18 @@
+package objektwerks
+
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.layout.VBox
+
+import Conf.*
+
+object View:
+  val sceneGraph = new Scene {
+    root = new VBox {
+      prefWidth = windowWidth
+      prefHeight = windowHeight
+      spacing = 6
+      padding = Insets(6)
+      children = List()
+    }
+  }
