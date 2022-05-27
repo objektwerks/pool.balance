f994ff991f52925b0a497c85673af74fd3c05e18
objektwerks <objektwerks@runbox.com>
2022-05-27 19:46:18 -0400

    refatored entity

--- src/main/scala/pool/Entity.scala
+++ src/main/scala/pool/Entity.scala
@@ -5,18 +5,17 @@ import java.time.format.DateTimeFormatter
 
 sealed trait Entity:
   val id: Int = 0
-  val dateCreated: LocalDate = LocalDate.now
-  val timeCreated: LocalTime = LocalTime.now
-
-  def newDateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-
-  def newTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("hh:mm")
 
 final case class Pool(volume: Int = 10000) extends Entity
 
 sealed trait Measurement extends Entity:
   val value: Double = 0.0
+  val dateCreated: LocalDate = LocalDate.now
+  val timeCreated: LocalTime = LocalTime.now
 
+  def newDateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
+  def newTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("hh:mm")
+  
 final case class FreeChlorine() extends Measurement
 
 final case class CombinedChlorine() extends Measurement
