01da00e6ccbe16a7b8aa65c387f29bb1130628f7
objektwerks <objektwerks@runbox.com>
2022-05-27 19:02:26 -0400

    edited store test

--- src/test/scala/pool/StoreTest.scala
+++ src/test/scala/pool/StoreTest.scala
@@ -7,7 +7,8 @@ import org.scalatest.matchers.should.Matchers
 
 final class StoreTest extends AnyFunSuite with Matchers:
   val context = Context( ConfigFactory.load("test.conf") )
+  val store = context.store
 
   test("store") {
-    context.store.ping shouldBe true
+    store.ping shouldBe true
   }
