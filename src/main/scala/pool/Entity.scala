59d556682b6127d473efc5a560492a59f1ef1424
objektwerks <objektwerks@runbox.com>
2022-05-27 13:13:32 -0400

    added combined and total chlorine

--- src/main/scala/pool/Entity.scala
+++ src/main/scala/pool/Entity.scala
@@ -15,3 +15,7 @@ sealed trait Entity:
 final case class Pool(volume: Int = 10000) extends Entity
 
 final case class FreeChlorine(value: Double)
+
+final case class CombinedChlorine(value: Double)
+
+final case class TotalChlorine(value: Double)
