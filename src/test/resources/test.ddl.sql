638100a477f5c7855a930ba80deb05fd33f3c6cf
objektwerks <objektwerks@runbox.com>
2022-05-27 13:51:47 -0400

    added test.ddl.sql

---
+++ src/test/resources/test.ddl.sql
@@ -0,0 +1,61 @@
+drop all objects;
+create table pool (
+  id int primary key auto_increment,
+  date_created date not null,
+  time_created time not null,
+  volume double not null
+);
+create table free_chlorine (
+  id int primary key auto_increment,
+  date_created date not null,
+  time_created time not null,
+  value double not null
+);
+create table combined_chlorine (
+  id int primary key auto_increment,
+  date_created date not null,
+  time_created time not null,
+  value double not null
+);
+create table total_chlorine (
+  id int primary key auto_increment,
+  date_created date not null,
+  time_created time not null,
+  value double not null
+);
+create table ph (
+  id int primary key auto_increment,
+  date_created date not null,
+  time_created time not null,
+  value double not null
+);
+create table calcium_hardness (
+  id int primary key auto_increment,
+  date_created date not null,
+  time_created time not null,
+  value double not null
+);
+create table total_alkalinity (
+  id int primary key auto_increment,
+  date_created date not null,
+  time_created time not null,
+  value double not null
+);
+create table cyanuric_acid (
+  id int primary key auto_increment,
+  date_created date not null,
+  time_created time not null,
+  value double not null
+);
+create table total_bromine (
+  id int primary key auto_increment,
+  date_created date not null,
+  time_created time not null,
+  value double not null
+);
+create table temperature (
+  id int primary key auto_increment,
+  date_created date not null,
+  time_created time not null,
+  value double not null
+);
