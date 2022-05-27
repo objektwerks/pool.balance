3fa7c6064f36e4e8de2a1c8ca499e392eb659b35
objektwerks <objektwerks@runbox.com>
2022-05-27 19:04:30 -0400

    reformatted App

--- src/main/scala/pool/App.scala
+++ src/main/scala/pool/App.scala
@@ -21,8 +21,7 @@ object App extends JFXApp3 with LazyLogging:
       minHeight = context.windowHeight
       icons.add(context.logo)
     }
-    
+
     logger.info("App started.")
 
-  override def stopApp(): Unit =
-    logger.info("App stopped.")
+  override def stopApp(): Unit = logger.info("App stopped.")
