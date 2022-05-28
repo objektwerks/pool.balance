49bba2c36f78deec117a631cb0d653b40031b41e
objektwerks <objektwerks@runbox.com>
2022-05-28 15:24:43 -0400

    renamed date and time columns

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -4,55 +4,55 @@ create table if not exists pool (
 );
 create table if not exists free_chlorine (
   id int primary key auto_increment not null,
-  date_created date not null,
-  time_created time not null,
+  date_measured date not null,
+  time_measured time not null,
   value double not null
 );
 create table if not exists combined_chlorine (
   id int primary key auto_increment not null,
-  date_created date not null,
-  time_created time not null,
+  date_measured date not null,
+  time_measured time not null,
   value double not null
 );
 create table if not exists total_chlorine (
   id int primary key auto_increment not null,
-  date_created date not null,
-  time_created time not null,
+  date_measured date not null,
+  time_measured time not null,
   value double not null
 );
 create table if not exists ph (
   id int primary key auto_increment not null,
-  date_created date not null,
-  time_created time not null,
+  date_measured date not null,
+  time_measured time not null,
   value double not null
 );
 create table if not exists calcium_hardness (
   id int primary key auto_increment not null,
-  date_created date not null,
-  time_created time not null,
+  date_measured date not null,
+  time_measured time not null,
   value double not null
 );
 create table if not exists total_alkalinity (
   id int primary key auto_increment not null,
-  date_created date not null,
-  time_created time not null,
+  date_measured date not null,
+  time_measured time not null,
   value double not null
 );
 create table if not exists cyanuric_acid (
   id int primary key auto_increment not null,
-  date_created date not null,
-  time_created time not null,
+  date_measured date not null,
+  time_measured time not null,
   value double not null
 );
 create table if not exists total_bromine (
   id int primary key auto_increment not null,
-  date_created date not null,
-  time_created time not null,
+  date_measured date not null,
+  time_measured time not null,
   value double not null
 );
 create table if not exists temperature (
   id int primary key auto_increment not null,
-  date_created date not null,
-  time_created time not null,
+  date_measured date not null,
+  time_measured time not null,
   value double not null
 );
