1cdb5f5cc0a2bd05d8b23eb4fd93628b6c2e0c02
objektwerks <objektwerks@runbox.com>
2022-05-30 16:03:30 -0400

    enhanced ddl.sql

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,59 +1,68 @@
 create table if not exists pool (
-  id long primary key auto_increment not null,
+  id long primary key auto_increment,
   name varchar not null,
   build date not null,
   volume double not null
 );
 create table if not exists free_chlorine (
-  id long primary key auto_increment not null,
+  id long primary key auto_increment,
+  pool_id long references pool(id),
   date_measured date not null,
   time_measured time not null,
   measurement double not null
 );
 create table if not exists combined_chlorine (
-  id long primary key auto_increment not null,
+  id long primary key auto_increment,
+  pool_id long references pool(id),
   date_measured date not null,
   time_measured time not null,
   measurement double not null
 );
 create table if not exists total_chlorine (
-  id long primary key auto_increment not null,
+  id long primary key auto_increment,
+  pool_id long references pool(id),
   date_measured date not null,
   time_measured time not null,
   measurement double not null
 );
 create table if not exists ph (
-  id long primary key auto_increment not null,
+  id long primary key auto_increment,
+  pool_id long references pool(id),
   date_measured date not null,
   time_measured time not null,
   measurement double not null
 );
 create table if not exists calcium_hardness (
-  id long primary key auto_increment not null,
+  id long primary key auto_increment,
+  pool_id long references pool(id),
   date_measured date not null,
   time_measured time not null,
   measurement double not null
 );
 create table if not exists total_alkalinity (
-  id long primary key auto_increment not null,
+  id long primary key auto_increment,
+  pool_id long references pool(id),
   date_measured date not null,
   time_measured time not null,
   measurement double not null
 );
 create table if not exists cyanuric_acid (
-  id long primary key auto_increment not null,
+  id long primary key auto_increment,
+  pool_id long references pool(id),
   date_measured date not null,
   time_measured time not null,
   measurement double not null
 );
 create table if not exists total_bromine (
-  id long primary key auto_increment not null,
+  id long primary key auto_increment,
+  pool_id long references pool(id),
   date_measured date not null,
   time_measured time not null,
   measurement double not null
 );
 create table if not exists temperature (
-  id long primary key auto_increment not null,
+  id long primary key auto_increment,
+  pool_id long references pool(id),
   date_measured date not null,
   time_measured time not null,
   measurement double not null
