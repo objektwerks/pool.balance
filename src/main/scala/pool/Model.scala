99ab4f1de3105c633d8bd681ba8f1a5dfaa1a154
objektwerks <objektwerks@runbox.com>
2022-05-30 10:19:12 -0400

    enhanced model

--- src/main/scala/pool/Model.scala
+++ src/main/scala/pool/Model.scala
@@ -13,18 +13,16 @@ package pool
 */
 final class Model(context: Context):
   def pools(): List[Pool] = List[Pool]()
-
   def add(pool: Pool): Int = 0
-
   def update(pool: Pool): Unit = ()
 
   def freeChlorines(): List[FreeChlorine] = List[FreeChlorine]()
-
   def add(freeChlorine: FreeChlorine): Int = 0
-
   def update(freeChlorine: FreeChlorine): Unit = ()
 
   def combinedChlorines(): List[CombinedChlorine] = List[CombinedChlorine]()
+  def add(combinedChlorine: CombinedChlorine): Int = 0
+  def update(combinedChlorine: CombinedChlorine): Unit = ()
 
   def totalChlorines(): List[TotalChlorine] = List[TotalChlorine]()
 
