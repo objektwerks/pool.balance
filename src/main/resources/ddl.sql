43dc8a48865059b8006eaa5bf384f93559b1c1ac
objektwerks <objektwerks@runbox.com>
2022-05-30 11:21:30 -0400

    refactored all measurement tables; enhanced pool table

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,58 +1,60 @@
 create table if not exists pool (
   id int primary key auto_increment not null,
+  name varchar not null,
+  build date not null,
   volume double not null
 );
 create table if not exists free_chlorine (
   id int primary key auto_increment not null,
   date_measured date not null,
   time_measured time not null,
-  value double not null
+  measurement double not null
 );
 create table if not exists combined_chlorine (
   id int primary key auto_increment not null,
   date_measured date not null,
   time_measured time not null,
-  value double not null
+  measurement double not null
 );
 create table if not exists total_chlorine (
   id int primary key auto_increment not null,
   date_measured date not null,
   time_measured time not null,
-  value double not null
+  measurement double not null
 );
 create table if not exists ph (
   id int primary key auto_increment not null,
   date_measured date not null,
   time_measured time not null,
-  value double not null
+  measurement double not null
 );
 create table if not exists calcium_hardness (
   id int primary key auto_increment not null,
   date_measured date not null,
   time_measured time not null,
-  value double not null
+  measurement double not null
 );
 create table if not exists total_alkalinity (
   id int primary key auto_increment not null,
   date_measured date not null,
   time_measured time not null,
-  value double not null
+  measurement double not null
 );
 create table if not exists cyanuric_acid (
   id int primary key auto_increment not null,
   date_measured date not null,
   time_measured time not null,
-  value double not null
+  measurement double not null
 );
 create table if not exists total_bromine (
   id int primary key auto_increment not null,
   date_measured date not null,
   time_measured time not null,
-  value double not null
+  measurement double not null
 );
 create table if not exists temperature (
   id int primary key auto_increment not null,
   date_measured date not null,
   time_measured time not null,
-  value double not null
+  measurement double not null
 );
