5d960d68d9b442b49dd728a36e58205cc98cffb8
objektwerks <objektwerks@runbox.com>
2022-05-30 09:59:37 -0400

    enhanced model

--- src/main/scala/pool/Model.scala
+++ src/main/scala/pool/Model.scala
@@ -1,4 +1,19 @@
 package pool
 
+/*
+2. FreeChlorine
+3. CombinedChlorine
+4. TotalChlorine
+5. pH
+6. CalciumHardness
+7. TotalAlkalinity
+8. CyanuricAcid
+9. TotalBromine
+10. Temperature
+*/
 final class Model(context: Context):
   def listPools(): List[Pool] = List[Pool]()
+
+  def listFreeChlorine(): List[FreeChlorine] = List[FreeChlorine]()
+
+  
