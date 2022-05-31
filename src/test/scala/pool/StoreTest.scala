5998966e3823a6a97807d0e473166a20f10af90e
objektwerks <objektwerks@runbox.com>
2022-05-31 12:40:51 -0400

    added logging to store test

--- src/test/scala/pool/StoreTest.scala
+++ src/test/scala/pool/StoreTest.scala
@@ -1,15 +1,17 @@
 package pool
 
 import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import java.time.LocalDate
 
-final class StoreTest extends AnyFunSuite with Matchers:
+final class StoreTest extends AnyFunSuite with Matchers with LazyLogging:
   val store = Context( ConfigFactory.load("test.conf") ).store
 
   test("pool") {
+    logger.info("Pool store test started ...")
     val pool = Pool(name = "pool-a", built = LocalDate.now(), volume = 10000)
 
     val added = store.add(pool)
@@ -21,4 +23,5 @@ final class StoreTest extends AnyFunSuite with Matchers:
     val list = store.pools()
     list.length shouldBe 1
     list.head shouldBe updated
+    logger.info("Pool store test stopped.")
   }
