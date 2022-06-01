b5cdbe39ac2e5e3e2cd721f41f0e913378e59787
objektwerks <objektwerks@runbox.com>
2022-06-01 13:47:22 -0400

    refacotored enum test

--- src/test/scala/pool/EnumTest.scala
+++ src/test/scala/pool/EnumTest.scala
@@ -5,11 +5,11 @@ import org.scalatest.matchers.should.Matchers
 
 class EnumTest extends AnyFunSuite with Matchers:
   test("uom") {
-    uom.gl.toString shouldBe "gl"
-    uom.kg.toString shouldBe "kg"
-    uom.g.toString shouldBe "g"
-    uom.l.toString shouldBe "l"
-    uom.ml.toString shouldBe "ml"
-    uom.lbs.toString shouldBe "lbs"
-    uom.oz.toString shouldBe "oz"
-  }
+    uom.valueOf(uom.gl.toString) shouldBe uom.gl
+    uom.valueOf(uom.kg.toString) shouldBe uom.kg
+    uom.valueOf(uom.g.toString) shouldBe uom.g
+    uom.valueOf(uom.l.toString) shouldBe uom.l
+    uom.valueOf(uom.ml.toString) shouldBe uom.ml
+    uom.valueOf(uom.lbs.toString) shouldBe uom.lbs
+    uom.valueOf(uom.oz.toString) shouldBe uom.oz
+ }
