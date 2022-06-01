e5a0169cfa9a87aa5b90c7d6e4a0199552b031a6
objektwerks <objektwerks@runbox.com>
2022-06-01 19:23:06 -0400

    edited ddl sql

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -4,104 +4,18 @@ create table if not exists pool (
   built date not null,
   volume double not null
 );
-create table if not exists free_chlorine (
+create table if not exists measurement (
   id long primary key auto_increment,
   pool_id long references pool(id),
+  typeof varchar not null,
   date_measured date not null,
   time_measured time not null,
   measurement double not null
 );
-create table if not exists combined_chlorine (
+create table if not exists chemical (
   id long primary key auto_increment,
   pool_id long references pool(id),
-  date_measured date not null,
-  time_measured time not null,
-  measurement double not null
-);
-create table if not exists total_chlorine (
-  id long primary key auto_increment,
-  pool_id long references pool(id),
-  date_measured date not null,
-  time_measured time not null,
-  measurement double not null
-);
-create table if not exists ph (
-  id long primary key auto_increment,
-  pool_id long references pool(id),
-  date_measured date not null,
-  time_measured time not null,
-  measurement double not null
-);
-create table if not exists calcium_hardness (
-  id long primary key auto_increment,
-  pool_id long references pool(id),
-  date_measured date not null,
-  time_measured time not null,
-  measurement double not null
-);
-create table if not exists total_alkalinity (
-  id long primary key auto_increment,
-  pool_id long references pool(id),
-  date_measured date not null,
-  time_measured time not null,
-  measurement double not null
-);
-create table if not exists cyanuric_acid (
-  id long primary key auto_increment,
-  pool_id long references pool(id),
-  date_measured date not null,
-  time_measured time not null,
-  measurement double not null
-);
-create table if not exists total_bromine (
-  id long primary key auto_increment,
-  pool_id long references pool(id),
-  date_measured date not null,
-  time_measured time not null,
-  measurement double not null
-);
-create table if not exists temperature (
-  id long primary key auto_increment,
-  pool_id long references pool(id),
-  date_measured date not null,
-  time_measured time not null,
-  measurement double not null
-);
-create table if not exists liquid_chlorine (
-  id long primary key auto_increment,
-  pool_id long references pool(id),
-  date_added date not null,
-  time_added time not null,
-  amount double not null,
-  unit varchar not null
-);
-create table if not exists trichlor (
-  id long primary key auto_increment,
-  pool_id long references pool(id),
-  date_added date not null,
-  time_added time not null,
-  amount double not null,
-  unit varchar not null
-);
-create table if not exists dichlor (
-  id long primary key auto_increment,
-  pool_id long references pool(id),
-  date_added date not null,
-  time_added time not null,
-  amount double not null,
-  unit varchar not null
-);
-create table if not exists calcium_hypochlorite (
-  id long primary key auto_increment,
-  pool_id long references pool(id),
-  date_added date not null,
-  time_added time not null,
-  amount double not null,
-  unit varchar not null
-);
-create table if not exists stabilizer (
-  id long primary key auto_increment,
-  pool_id long references pool(id),
+  typeof varchar not null,
   date_added date not null,
   time_added time not null,
   amount double not null,
