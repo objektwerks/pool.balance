0271195ea310b8afc530ffce454fe02915935474
objektwerks <objektwerks@runbox.com>
2022-06-03 10:51:34 -0400

    enhanced entity to format new local dates

--- src/main/scala/pool/Entity.scala
+++ src/main/scala/pool/Entity.scala
@@ -32,7 +32,7 @@ final case class Cleaning(id: Long = 0,
                           pumpBasket: Boolean = false,
                           pumpFilter: Boolean = false,
                           vacuum: Boolean = false,
-                          dateCleaned: LocalDate = LocalDate.now) extends Entity
+                          dateCleaned: LocalDate = Entity.formatLocalDate(LocalDate.now)) extends Entity
 
 enum typeOfMeasurement:
   case freeChlorine, combinedChlorine, totalChlorine, pH, calciumHardness, totalAlkalinity, cyanuricAcid, totalBromine, temperature
@@ -43,7 +43,7 @@ object typeOfMeasurement:
 final case class Measurement(id: Long = 0,
                              poolId: Long,
                              typeof: typeOfMeasurement,
-                             dateMeasured: LocalDate = LocalDate.now,
+                             dateMeasured: LocalDate = Entity.formatLocalDate(LocalDate.now),
                              timeMeasured: LocalTime = Entity.formatLocalTime(LocalTime.now),
                              measurement: Double ) extends Entity:
   given measurementOrdering: Ordering[Measurement] = Ordering.by(m => (m.dateMeasured.toEpochDay, m.timeMeasured.toSecondOfDay))
@@ -58,7 +58,7 @@ object typeOfChemical:
 final case class Chemical(id: Long = 0,
                                 poolId: Long,
                                 typeof: typeOfChemical,
-                                dateAdded: LocalDate = LocalDate.now,
+                                dateAdded: LocalDate = Entity.formatLocalDate(LocalDate.now),
                                 timeAdded: LocalTime = Entity.formatLocalTime(LocalTime.now),
                                 amount: Double, 
                                 unit: unitOfMeasure) extends Entity:
