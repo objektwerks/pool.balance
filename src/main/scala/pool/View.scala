43cd91c3e415c7ac89cfa39445b60638db9a6db4
objektwerks <objektwerks@runbox.com>
2022-06-09 12:32:23 -0400

    refactored view

--- src/main/scala/pool/View.scala
+++ src/main/scala/pool/View.scala
@@ -9,12 +9,14 @@ import pool.pane.PoolPane
 final class View(context: Context):
   val poolPane = PoolPane(context)
 
+  val contentPane = new VBox {
+    prefWidth = context.windowWidth
+    prefHeight = context.windowHeight
+    spacing = 6
+    padding = Insets(6)
+    children = List(poolPane)
+  }
+
   val scene = new Scene {
-    root = new VBox {
-      prefWidth = context.windowWidth
-      prefHeight = context.windowHeight
-      spacing = 6
-      padding = Insets(6)
-      children = List(poolPane)
-    }
+    root = contentPane
   }
