ec539b0cca3d6993ae27534d78fe6f17f16d2253
objektwerks <objektwerks@runbox.com>
2022-06-09 12:26:28 -0400

    added pool pane to view

--- src/main/scala/pool/View.scala
+++ src/main/scala/pool/View.scala
@@ -4,13 +4,17 @@ import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.layout.VBox
 
+import pool.pane.PoolPane
+
 final class View(context: Context):
+  val poolPane = PoolPane(context)
+
   val scene = new Scene {
     root = new VBox {
       prefWidth = context.windowWidth
       prefHeight = context.windowHeight
       spacing = 6
       padding = Insets(6)
-      children = List()
+      children = List(poolPane)
     }
   }
