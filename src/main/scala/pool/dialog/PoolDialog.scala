c5f072dec6c5209eabd1899cf2d87fbcd35532ff
objektwerks <objektwerks@runbox.com>
2022-06-09 14:34:14 -0400

    refactored pool dialog imports

--- src/main/scala/pool/dialog/PoolDialog.scala
+++ src/main/scala/pool/dialog/PoolDialog.scala
@@ -8,6 +8,7 @@ import scalafx.scene.control.ButtonBar.ButtonData
 
 import pool.{App, Context, Entity, Pool}
 import pool.unitOfMeasure
+import pool.Entity.*
 
 class PoolDialog(context: Context, pool: Pool) extends Dialog[Pool]:
   initOwner(App.stage)
@@ -45,8 +46,8 @@ class PoolDialog(context: Context, pool: Pool) extends Dialog[Pool]:
   dialog.content = ControlGridPane(controls)
   
   nameTextField.text.onChange { (_, _, newValue) => saveButton.disable = newValue.trim.isEmpty }
-  builtTextField.text.onChange { (_, oldValue, newValue) => if (Entity.isNotInt(newValue)) builtTextField.text.value = oldValue }
-  volumeTextField.text.onChange { (_, oldValue, newValue) => if (Entity.isNotInt(newValue)) volumeTextField.text.value = oldValue }
+  builtTextField.text.onChange { (_, oldValue, newValue) => if (isNotInt(newValue)) builtTextField.text.value = oldValue }
+  volumeTextField.text.onChange { (_, oldValue, newValue) => if (isNotInt(newValue)) volumeTextField.text.value = oldValue }
   unitComboBox.value.onChange { (_, _, newValue) => saveButton.disable = newValue.trim.isEmpty }
 
   resultConverter = dialogButton => {
