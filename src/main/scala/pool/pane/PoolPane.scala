3e6cfe27cb185703e7b1a48effeb10b88cc9462c
objektwerks <objektwerks@runbox.com>
2022-06-09 08:30:35 -0400

    added buttons and toolbar to pool pane

--- src/main/scala/pool/pane/PoolPane.scala
+++ src/main/scala/pool/pane/PoolPane.scala
@@ -1,8 +1,10 @@
 package pool.pane
 
+import scalafx.Includes._
 import scalafx.collections.ObservableBuffer
-import scalafx.scene.layout.VBox
-import scalafx.scene.control.{Label, TableColumn, TableView}
+import scalafx.geometry.Insets
+import scalafx.scene.control.{Button, Label, SelectionMode, TableColumn, TableView}
+import scalafx.scene.layout.{HBox, VBox}
 
 import pool.{Context, Pool}
 
@@ -36,3 +38,21 @@ class PoolPane(context: Context) extends VBox:
       case Right(pools) => items = pools
       case Left(_) => items = ObservableBuffer[Pool]()
   }
+  poolTableView.selectionModel().selectionModeProperty.value = SelectionMode.Single
+
+  val poolAddButton = new Button {
+    graphic = context.addImage
+  }
+
+  val poolEditButton = new Button {
+    graphic = context.editImage
+    disable = true
+  }
+
+  val poolToolBar = new HBox {
+    spacing = 6; children = List(poolAddButton, poolEditButton)
+  }
+
+  spacing = 6
+  padding = Insets(6)
+  children = List(poolLabel, poolTableView, poolToolBar)
