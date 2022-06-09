c9e62d5fd408ca033271a45152fa5de5321b2038
objektwerks <objektwerks@runbox.com>
2022-06-09 17:16:16 -0400

    set alignment on menu and dashboard panes

--- src/main/scala/pool/pane/DashboardPane.scala
+++ src/main/scala/pool/pane/DashboardPane.scala
@@ -1,9 +1,10 @@
 package pool.pane
 
-import scalafx.geometry.Orientation
+import scalafx.geometry.{Orientation, Pos}
 import scalafx.scene.layout.TilePane
 
 import pool.Context
 
 class DashboardPane(context: Context) extends TilePane:
   orientation = Orientation.Horizontal
+  alignment = Pos.Center
