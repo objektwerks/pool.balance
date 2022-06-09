b2b4746ae4e150b0ee35c2ca52efb49e22f52f78
objektwerks <objektwerks@runbox.com>
2022-06-09 08:48:09 -0400

    added event handlers and add/update methods to pool pane

--- src/main/scala/pool/pane/PoolPane.scala
+++ src/main/scala/pool/pane/PoolPane.scala
@@ -6,10 +6,14 @@ import scalafx.geometry.Insets
 import scalafx.scene.control.{Button, Label, SelectionMode, TableColumn, TableView}
 import scalafx.scene.layout.{HBox, VBox}
 
-import pool.{Context, Pool}
+import pool.{Context, Pool, unitOfMeasure}
+import pool.dialog.PoolDialog
 
 class PoolPane(context: Context) extends VBox:
   val model = context.model
+
+  spacing = 6
+  padding = Insets(6)
 
   val poolLabel = new Label {
     text = context.labelPools
@@ -38,7 +42,6 @@ class PoolPane(context: Context) extends VBox:
       case Right(pools) => items = pools
       case Left(_) => items = ObservableBuffer[Pool]()
   }
-  poolTableView.selectionModel().selectionModeProperty.value = SelectionMode.Single
 
   val poolAddButton = new Button {
     graphic = context.addImage
@@ -53,6 +56,34 @@ class PoolPane(context: Context) extends VBox:
     spacing = 6; children = List(poolAddButton, poolEditButton)
   }
 
-  spacing = 6
-  padding = Insets(6)
   children = List(poolLabel, poolTableView, poolToolBar)
+
+  poolTableView.selectionModel().selectionModeProperty.value = SelectionMode.Single
+  poolTableView.selectionModel().selectedItemProperty().addListener { (_, _, selectedPool) =>
+    // model.update executes a remove and add on items. the remove passes a null selectedPool!
+    if (selectedPool != null) {
+      model.selectedPoolId.value = selectedPool.id
+      poolEditButton.disable = false
+    }
+  }
+
+  poolAddButton.onAction = { _ => add() }
+
+  poolEditButton.onAction = { _ => update() }
+
+  def add(): Unit =
+    PoolDialog(context, Pool(name = "", built = 0, volume = 0, unit = unitOfMeasure.gl)).showAndWait() match
+      case Some(Pool(id, name, built, volume, unit)) =>
+        model.add(Pool(id, name, built, volume, unit)) match
+          case Right(pool) => poolTableView.selectionModel().select(pool)
+          case Left(_) =>        
+      case _ =>
+
+  def update(): Unit =
+    val selectedIndex = poolTableView.selectionModel().getSelectedIndex
+    val pool = poolTableView.selectionModel().getSelectedItem.pool
+    PoolDialog(context, pool).showAndWait() match
+      case Some(Pool(id, name, built, volume, unit)) =>
+        model.update(selectedIndex, Pool(id, name, built, volume, unit))
+        poolTableView.selectionModel().select(selectedIndex)
+      case _ =>
