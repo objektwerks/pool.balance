1c9a0f7a878a51de99d9b9f72305062d3117bf56
objektwerks <objektwerks@runbox.com>
2022-06-09 12:37:56 -0400

    enhanced view

--- src/main/scala/pool/View.scala
+++ src/main/scala/pool/View.scala
@@ -2,19 +2,27 @@ package pool
 
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.layout.VBox
+import scalafx.scene.layout.{HBox, VBox}
 
 import pool.pane.PoolPane
 
 final class View(context: Context):
   val poolPane = PoolPane(context)
 
-  val contentPane = new VBox {
+  val westPane = new VBox {
+    children = List(poolPane)
+  }
+
+  val eastPane = new VBox {
+    children = List()
+  }
+
+  val contentPane = new HBox {
     prefWidth = context.windowWidth
     prefHeight = context.windowHeight
     spacing = 6
     padding = Insets(6)
-    children = List(poolPane)
+    children = List(westPane, eastPane)
   }
 
   val scene = new Scene {
