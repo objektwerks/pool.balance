734dec997927ff23cafba19648c5fdb22232c64f
objektwerks <objektwerks@runbox.com>
2022-06-09 07:53:27 -0400

    added lable to pool pane

--- src/main/scala/pool/pane/PoolPane.scala
+++ src/main/scala/pool/pane/PoolPane.scala
@@ -3,5 +3,9 @@ package pool.pane
 import pool.Context
 
 import scalafx.scene.layout.VBox
+import scalafx.scene.control.Label
 
-class PoolPane(context: Context) extends VBox
+class PoolPane(context: Context) extends VBox:
+  val poolLabel = new Label {
+    text = context.labelPools
+  }
