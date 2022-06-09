11e5871eb2039caa3f97b957d1fe2f2d02dd68c3
objektwerks <objektwerks@runbox.com>
2022-06-09 11:10:31 -0400

    added fields to pool dialog

--- src/main/scala/pool/dialog/PoolDialog.scala
+++ src/main/scala/pool/dialog/PoolDialog.scala
@@ -1,7 +1,24 @@
 package pool.dialog
 
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.control.{ComboBox, Dialog, TextField}
+
 import pool.{Context, Pool}
 
-import scalafx.scene.control.Dialog
+class PoolDialog(context: Context, pool: Pool) extends Dialog[Pool]:
+  val nameTextField = new TextField {
+    text = pool.name
+  }
 
-class PoolDialog(context: Context, pool: Pool) extends Dialog[Pool]
+  val builtTextField = new TextField {
+    text = pool.built.toString
+  }
+
+  val volumeTextField = new TextField {
+    text = pool.volume.toString
+  }
+  
+  val unitComboBox = new ComboBox[String] {
+  	items = ObservableBuffer.from(context.listUnits)
+  	value = pool.unit.toString
+  }
