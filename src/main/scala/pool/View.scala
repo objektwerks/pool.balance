5b2d09e60969c4df1916fda11f47987fdcc2913a
objektwerks <objektwerks@runbox.com>
2022-06-09 17:45:36 -0400

    changed menu pane to tabbed pane

--- src/main/scala/pool/View.scala
+++ src/main/scala/pool/View.scala
@@ -4,20 +4,19 @@ import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.layout.{HBox, VBox}
 
-import pool.pane.{ContentPane, DashboardPane, MenuPane, PoolPane}
+import pool.pane.{ContentPane, DashboardPane, TabbedPane, PoolPane}
 
 final class View(context: Context):
   val dashboardPane = DashboardPane(context)
   val contentPane = ContentPane(context)
-  val poolPane = PoolPane(context)
-  val menuPane = MenuPane(context)
-
   val eastPane = new VBox {
     children = List(dashboardPane, contentPane)
   }
 
+  val poolPane = PoolPane(context)
+  val tabbedPane = TabbedPane(context)
   val westPane = new VBox {
-    children = List(poolPane, menuPane)
+    children = List(poolPane, tabbedPane)
   }
 
   val rootPane = new HBox {
---
+++ src/main/scala/pool/pane/TabbedPane.scala
@@ -0,0 +1,7 @@
+package pool.pane
+
+import scalafx.scene.control.TabPane
+
+import pool.Context
+
+class TabbedPane(context: Context) extends TabPane
