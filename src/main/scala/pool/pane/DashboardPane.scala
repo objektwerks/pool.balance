d1df0bd0272d373b6c87155267724b6fa7d09794
objektwerks <objektwerks@runbox.com>
2022-06-10 11:56:56 -0400

    refactored dashboard pane layout

--- src/main/scala/pool/pane/DashboardPane.scala
+++ src/main/scala/pool/pane/DashboardPane.scala
@@ -1,15 +1,15 @@
 package pool.pane
 
-import scalafx.geometry.{Insets, Orientation, Pos}
-import scalafx.scene.layout.GridPane
+import scalafx.geometry.Insets
+import scalafx.scene.layout.VBox
 
 import pool.Context
 
-class DashboardPane(context: Context) extends GridPane:
-  alignment = Pos.Center
+class DashboardPane(context: Context) extends VBox:
+  spacing = 6
   padding = Insets(6)
 
-  add(FreeChlorinePane(context), columnIndex = 0, rowIndex = 0, colspan = 1, rowspan = 1)
-  add(PhPane(context), columnIndex = 0, rowIndex = 1, colspan = 1, rowspan = 1)
-  add(TotalAlkalinityPane(context), columnIndex = 0, rowIndex = 2, colspan = 1, rowspan = 1)
-  add(CalciumHardnessPane(context), columnIndex = 0, rowIndex = 3, colspan = 1, rowspan = 1)
+  children = List(FreeChlorinePane(context),
+                  PhPane(context),
+                  TotalAlkalinityPane(context),
+                  CalciumHardnessPane(context))
