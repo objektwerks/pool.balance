b0aca2188c0744e027da0fe123b108340f34d7b8
objektwerks <objektwerks@runbox.com>
2022-06-10 18:08:04 -0400

    added measurement dialog

---
+++ src/main/scala/pool/dialog/ChemicalDialog.scala
@@ -0,0 +1,16 @@
+package pool.dialog
+
+import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.layout.Region
+import scalafx.scene.control.{ButtonType, ComboBox, Dialog, TextField}
+import scalafx.scene.control.ButtonBar.ButtonData
+
+import pool.{App, Context, Entity, Chemical}
+import pool.unitOfMeasure
+import pool.Entity.*
+
+class ChemicalDialog(context: Context, chemical: Chemical) extends Dialog[Chemical]:
+  initOwner(App.stage)
+  title = context.windowTitle
+  headerText = context.dialogChemical
---
+++ src/main/scala/pool/dialog/MeasurementDialog.scala
@@ -0,0 +1,16 @@
+package pool.dialog
+
+import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.layout.Region
+import scalafx.scene.control.{ButtonType, ComboBox, Dialog, TextField}
+import scalafx.scene.control.ButtonBar.ButtonData
+
+import pool.{App, Context, Entity, Measurement}
+import pool.unitOfMeasure
+import pool.Entity.*
+
+class MeasurementDialog(context: Context, measurement: Measurement) extends Dialog[Measurement]:
+  initOwner(App.stage)
+  title = context.windowTitle
+  headerText = context.dialogMeasurement
