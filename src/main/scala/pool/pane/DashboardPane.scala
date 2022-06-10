83b3518d1cb2a9b1ab976a87e2fa00ed9352654f
objektwerks <objektwerks@runbox.com>
2022-06-10 11:51:19 -0400

    switched to gridpane in dashboard pane

--- src/main/scala/pool/pane/DashboardPane.scala
+++ src/main/scala/pool/pane/DashboardPane.scala
@@ -9,7 +9,7 @@ class DashboardPane(context: Context) extends GridPane:
   alignment = Pos.Center
   padding = Insets(6)
 
-  add(FreeChlorinePane(context), columnIndex = 0, rowIndex = 1)
-  add(PhPane(context), columnIndex = 0, rowIndex = 2)
-  add(TotalAlkalinityPane(context), columnIndex = 0, rowIndex = 3)
-  add(CalciumHardnessPane(context), columnIndex = 0, rowIndex = 4)
+  add(FreeChlorinePane(context), columnIndex = 0, rowIndex = 0, colspan = 1, rowspan = 1)
+  add(PhPane(context), columnIndex = 0, rowIndex = 1, colspan = 1, rowspan = 1)
+  add(TotalAlkalinityPane(context), columnIndex = 0, rowIndex = 2, colspan = 1, rowspan = 1)
+  add(CalciumHardnessPane(context), columnIndex = 0, rowIndex = 3, colspan = 1, rowspan = 1)
