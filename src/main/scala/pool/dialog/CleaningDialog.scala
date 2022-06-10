850435783dd4014d3d0ca6c760af27bccc6a7c55
objektwerks <objektwerks@runbox.com>
2022-06-10 18:04:52 -0400

    added cleaning dialog

---
+++ src/main/scala/pool/dialog/CleaningDialog.scala
@@ -0,0 +1,16 @@
+package pool.dialog
+
+import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.layout.Region
+import scalafx.scene.control.{ButtonType, ComboBox, Dialog, TextField}
+import scalafx.scene.control.ButtonBar.ButtonData
+
+import pool.{App, Context, Entity, Cleaning}
+import pool.unitOfMeasure
+import pool.Entity.*
+
+class CleaningDialog(context: Context, cleaning: Cleaning) extends Dialog[Cleaning]:
+  initOwner(App.stage)
+  title = context.windowTitle
+  headerText = context.dialogCleaning
