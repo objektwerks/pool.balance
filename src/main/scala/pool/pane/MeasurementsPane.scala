df555d568464ea0d3af498043429c49cf784d0fc
objektwerks <objektwerks@runbox.com>
2022-06-12 08:14:04 -0400

    added imports to measurements pane

--- src/main/scala/pool/pane/MeasurementsPane.scala
+++ src/main/scala/pool/pane/MeasurementsPane.scala
@@ -1,10 +1,13 @@
 package pool.pane
 
+import scalafx.Includes.*
+import scalafx.collections.ObservableBuffer
 import scalafx.geometry.Insets
-import scalafx.scene.control.{Button, Label}
+import scalafx.scene.control.{Button, Label, SelectionMode, TableColumn, TableView}
 import scalafx.scene.layout.{HBox, VBox}
 
-import pool.Context
+import pool.{Measurement, Context}
+import pool.dialog.MeasurementDialog
 
 class MeasurementsPane(context: Context) extends VBox with AddEditToolbar(context):
   spacing = 6
