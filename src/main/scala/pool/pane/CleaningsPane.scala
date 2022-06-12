b2076dd933a39f7a619e22f240f2bddfe99a0aac
objektwerks <objektwerks@runbox.com>
2022-06-12 08:05:05 -0400

    added table view to cleanings pane

--- src/main/scala/pool/pane/CleaningsPane.scala
+++ src/main/scala/pool/pane/CleaningsPane.scala
@@ -1,10 +1,12 @@
 package pool.pane
 
+import scalafx.Includes.*
+import scalafx.collections.ObservableBuffer
 import scalafx.geometry.Insets
-import scalafx.scene.control.{Button, Label}
+import scalafx.scene.control.{Button, Label, SelectionMode, TableColumn, TableView}
 import scalafx.scene.layout.{HBox, VBox}
 
-import pool.Context
+import pool.{Cleaning, Context}
 
 class CleaningsPane(context: Context) extends VBox with AddEditToolbar(context):
   spacing = 6
@@ -16,4 +18,38 @@ class CleaningsPane(context: Context) extends VBox with AddEditToolbar(context):
     text = context.labelCleanings
   }
 
-  children = List(label, toolbar)
+  val tableView = new TableView[Cleaning]() {
+    columns ++= List(
+      new TableColumn[Cleaning, String] {
+        text = context.tableBrush
+        cellValueFactory = _.value.brushProperty
+      },
+      new TableColumn[Cleaning, String] {
+        text = context.tableNet
+        cellValueFactory = _.value.netProperty
+      },
+      new TableColumn[Cleaning, String] {
+        text = context.tableSkimmerBasket
+        cellValueFactory = _.value.skimmerBasketProperty
+      },
+      new TableColumn[Cleaning, String] {
+        text = context.tablePumpBasket
+        cellValueFactory = _.value.pumpBasketProperty
+      },
+      new TableColumn[Cleaning, String] {
+        text = context.tablePumpFilter
+        cellValueFactory = _.value.pumpFilterProperty
+      },
+      new TableColumn[Cleaning, String] {
+        text = context.tableVacuum
+        cellValueFactory = _.value.vacuumProperty
+      },
+      new TableColumn[Cleaning, String] {
+        text = context.tableCleaned
+        cellValueFactory = _.value.cleanedProperty
+      }
+    )
+    items = model.cleanings(model.selectedPoolId.value).fold( _ => ObservableBuffer[Cleaning](), cleanings => cleanings)
+  }
+
+  children = List(label, tableView, toolbar)
