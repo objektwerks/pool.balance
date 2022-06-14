aeb48d886373ac41b7c0d6d1564222dc1b5fb10d
objektwerks <objektwerks@runbox.com>
2022-06-14 09:18:33 -0400

    added cleaned date picker to cleaning dialog

--- src/main/scala/pool/dialog/CleaningDialog.scala
+++ src/main/scala/pool/dialog/CleaningDialog.scala
@@ -3,7 +3,7 @@ package pool.dialog
 import scalafx.Includes.*
 import scalafx.collections.ObservableBuffer
 import scalafx.scene.layout.Region
-import scalafx.scene.control.{ButtonType, ComboBox, Dialog, TextField}
+import scalafx.scene.control.{ButtonType, ComboBox, DatePicker, Dialog, TextField}
 import scalafx.scene.control.ButtonBar.ButtonData
 
 import pool.{App, Context, Entity, Cleaning}
@@ -14,3 +14,7 @@ class CleaningDialog(context: Context, cleaning: Cleaning) extends Dialog[Cleani
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.dialogCleaning
+
+  val cleanedDatePicker = new DatePicker {
+    value = cleaning.cleaned.toLocalDate
+  }
