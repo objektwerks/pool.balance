ff77a29ffb75e3362bcfd2e70c38c774ef13612b
objektwerks <objektwerks@runbox.com>
2022-06-14 09:32:28 -0400

    added check boxes to cleaning dialog

--- src/main/scala/pool/dialog/CleaningDialog.scala
+++ src/main/scala/pool/dialog/CleaningDialog.scala
@@ -3,7 +3,7 @@ package pool.dialog
 import scalafx.Includes.*
 import scalafx.collections.ObservableBuffer
 import scalafx.scene.layout.Region
-import scalafx.scene.control.{ButtonType, ComboBox, DatePicker, Dialog, TextField}
+import scalafx.scene.control.{ButtonType, CheckBox, ComboBox, DatePicker, Dialog, TextField}
 import scalafx.scene.control.ButtonBar.ButtonData
 
 import pool.{App, Context, Entity, Cleaning}
@@ -14,6 +14,30 @@ class CleaningDialog(context: Context, cleaning: Cleaning) extends Dialog[Cleani
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.dialogCleaning
+
+  val brushCheckBox = new CheckBox {
+    selected = cleaning.brush
+  }
+
+  val netCheckBox = new CheckBox {
+    selected = cleaning.net
+  }
+
+  val skimmerBasketCheckBox = new CheckBox {
+    selected = cleaning.skimmerBasket
+  }
+
+  val pumpBasketCheckBox = new CheckBox {
+    selected = cleaning.pumpBasket
+  }
+
+  val pumpFilterCheckBox = new CheckBox {
+    selected = cleaning.pumpFilter
+  }
+
+  val vacuumCheckBox = new CheckBox {
+    selected = cleaning.vacuum
+  }
 
   val cleanedDatePicker = new DatePicker {
     value = cleaning.cleaned.toLocalDate
