4fd09199873396726ee66116a19762284fb85f80
objektwerks <objektwerks@runbox.com>
2022-06-15 05:16:56 -0400

    removed change listeners in chemical dialog

--- src/main/scala/pool/dialog/ChemicalDialog.scala
+++ src/main/scala/pool/dialog/ChemicalDialog.scala
@@ -47,11 +47,6 @@ class ChemicalDialog(context: Context, chemical: Chemical) extends Dialog[Chemic
   pane.buttonTypes = List(saveButtonType, ButtonType.Cancel)
   val saveButton = pane.lookupButton(saveButtonType)
 
-  typeofComboBox.value.onChange { (_, _, newValue) => saveButton.disable = newValue.trim.isEmpty }
-  amountTextField.text.onChange { (_, _, newValue) => saveButton.disable = newValue.trim.isEmpty }
-  unitComboBox.value.onChange { (_, _, newValue) => saveButton.disable = newValue.trim.isEmpty }
-  addedDatePicker.value.onChange { (_, _, newValue) => saveButton.disable = false }
-
   resultConverter = dialogButton => {
     if dialogButton == saveButtonType then
       chemical.copy(
