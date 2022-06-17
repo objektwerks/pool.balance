8c4f8762693cf33219716d2710acbe59b60c306b
objektwerks <objektwerks@runbox.com>
2022-06-17 10:29:31 -0400

    edited dashboard pane

--- src/main/scala/pool/pane/dashboard/DashboardPane.scala
+++ src/main/scala/pool/pane/dashboard/DashboardPane.scala
@@ -1,7 +1,7 @@
 package pool.pane.dashboard
 
 import scalafx.geometry.Insets
-import scalafx.scene.layout.HBox
+import scalafx.scene.layout.{HBox, Priority}
 
 import pool.Context
 
@@ -10,11 +10,23 @@ class DashboardPane(context: Context) extends HBox:
   padding = Insets(6)
 
   val freeClorinePane = FreeChlorinePane(context)
+  HBox.setHgrow(freeClorinePane, Priority.Always)
+
   val phPane = PhPane(context)
+  HBox.setHgrow(freeClorinePane, Priority.Always)
+
   val totalAlkalinityPane = TotalAlkalinityPane(context)
+  HBox.setHgrow(freeClorinePane, Priority.Always)
+
   val calciumHardnessPane = CalciumHardnessPane(context)
+  HBox.setHgrow(freeClorinePane, Priority.Always)
+
   val cyanuricAcidPane = CyanuricAcidPane(context)
+  HBox.setHgrow(freeClorinePane, Priority.Always)
+
   val totalBrominePane = TotalBrominePane(context)
+  HBox.setHgrow(freeClorinePane, Priority.Always)
+
 
   children = List(freeClorinePane,
                   phPane, 
