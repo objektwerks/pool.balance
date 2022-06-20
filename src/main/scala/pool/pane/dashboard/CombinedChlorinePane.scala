0be8ec2f47743f7766227f8806649f1e777a680b
objektwerks <objektwerks@runbox.com>
2022-06-20 10:03:42 -0400

    added model change listener to combined chlorine pane

--- src/main/scala/pool/pane/dashboard/CombinedChlorinePane.scala
+++ src/main/scala/pool/pane/dashboard/CombinedChlorinePane.scala
@@ -4,3 +4,11 @@ import pool.Context
 
 class CombinedChlorinePane(context: Context) extends DashboardTitledPane(context):
   text = context.tableCombinedChlorine
+
+  context.model.currentCombinedChlorine.onChange { (_, _, newValue) =>
+    currentValue.text = newValue.toString
+  }
+
+  context.model.averageCombinedChlorine.onChange { (_, _, newValue) =>
+    currentAverage.text = newValue.toString
+  }
