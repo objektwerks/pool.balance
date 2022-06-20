5c19c710f3ff6ec343a7ebd8d2af3b23bfab5496
objektwerks <objektwerks@runbox.com>
2022-06-20 18:24:54 -0400

    replaced change listener with binding in ph pane

--- src/main/scala/pool/pane/dashboard/PhPane.scala
+++ src/main/scala/pool/pane/dashboard/PhPane.scala
@@ -4,11 +4,5 @@ import pool.Context
 
 class PhPane(context: Context) extends DashboardTitledPane(context):
   text = context.tablePh
-
-  context.model.currentPh.onChange { (_, _, newValue) =>
-    currentValue.text = newValue.toString
-  }
-
-  context.model.averagePh.onChange { (_, _, newValue) =>
-    currentAverage.text = newValue.toString
-  }
+  currentValue.text <== context.model.currentPh.asString
+  currentAverage.text <== context.model.averagePh.asString
