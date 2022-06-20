1dd8a856bdb24083e3e8fb0c1a5b75a8317201fa
objektwerks <objektwerks@runbox.com>
2022-06-20 18:25:37 -0400

    replaced change listener with binding in total alkalinity pane

--- src/main/scala/pool/pane/dashboard/TotalAlkalinityPane.scala
+++ src/main/scala/pool/pane/dashboard/TotalAlkalinityPane.scala
@@ -4,11 +4,5 @@ import pool.Context
 
 class TotalAlkalinityPane(context: Context) extends DashboardTitledPane(context):
   text = context.tableTotalAlkalinity
-
-  context.model.currentTotalAlkalinity.onChange { (_, _, newValue) =>
-    currentValue.text = newValue.toString
-  }
-
-  context.model.averageTotalAlkalinity.onChange { (_, _, newValue) =>
-    currentAverage.text = newValue.toString
-  }
+  currentValue.text <== context.model.currentTotalAlkalinity.asString
+  currentAverage.text <== context.model.averageTotalAlkalinity.asString
