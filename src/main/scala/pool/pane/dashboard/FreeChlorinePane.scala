4ffefcf9b4ccfc26a689ea00badc9ff628ae9fbf
objektwerks <objektwerks@runbox.com>
2022-06-20 10:08:25 -0400

    added model change listener to free chlorine pane

--- src/main/scala/pool/pane/dashboard/FreeChlorinePane.scala
+++ src/main/scala/pool/pane/dashboard/FreeChlorinePane.scala
@@ -4,3 +4,11 @@ import pool.Context
 
 class FreeChlorinePane(context: Context) extends DashboardTitledPane(context):
   text = context.tableFreeChlorine
+
+  context.model.currentFreeChlorine.onChange { (_, _, newValue) =>
+    currentValue.text = newValue.toString
+  }
+
+  context.model.averageFreeChlorine.onChange { (_, _, newValue) =>
+    currentAverage.text = newValue.toString
+  }
