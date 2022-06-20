299ca6ae8b6d69b6e94a853cf65f54732bb14556
objektwerks <objektwerks@runbox.com>
2022-06-20 10:09:29 -0400

    added mode change listener to total alkalinity pane

--- src/main/scala/pool/pane/dashboard/TotalAlkalinityPane.scala
+++ src/main/scala/pool/pane/dashboard/TotalAlkalinityPane.scala
@@ -4,3 +4,11 @@ import pool.Context
 
 class TotalAlkalinityPane(context: Context) extends DashboardTitledPane(context):
   text = context.tableTotalAlkalinity
+
+  context.model.currentTotalAlkalinity.onChange { (_, _, newValue) =>
+    currentValue.text = newValue.toString
+  }
+
+  context.model.averageTotalAlkalinity.onChange { (_, _, newValue) =>
+    currentAverage.text = newValue.toString
+  }
