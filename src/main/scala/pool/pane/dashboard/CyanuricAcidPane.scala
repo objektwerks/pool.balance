561e51cb982ba201040c6b5265b31a5f126c3000
objektwerks <objektwerks@runbox.com>
2022-06-20 10:07:46 -0400

    added model change listener to cyanuric acid pane

--- src/main/scala/pool/pane/dashboard/CyanuricAcidPane.scala
+++ src/main/scala/pool/pane/dashboard/CyanuricAcidPane.scala
@@ -4,3 +4,11 @@ import pool.Context
 
 class CyanuricAcidPane(context: Context) extends DashboardTitledPane(context):
   text = context.tableCyanuricAcid
+
+  context.model.currentCyanuricAcid.onChange { (_, _, newValue) =>
+    currentValue.text = newValue.toString
+  }
+
+  context.model.averageCyanuricAcid.onChange { (_, _, newValue) =>
+    currentAverage.text = newValue.toString
+  }
