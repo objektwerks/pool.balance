3a2d025b474bdc532a6626b60b7e7beac5882c55
objektwerks <objektwerks@runbox.com>
2022-06-20 10:09:01 -0400

    added model change listener to ph pane

--- src/main/scala/pool/pane/dashboard/PhPane.scala
+++ src/main/scala/pool/pane/dashboard/PhPane.scala
@@ -4,3 +4,11 @@ import pool.Context
 
 class PhPane(context: Context) extends DashboardTitledPane(context):
   text = context.tablePh
+
+  context.model.currentPh.onChange { (_, _, newValue) =>
+    currentValue.text = newValue.toString
+  }
+
+  context.model.averagePh.onChange { (_, _, newValue) =>
+    currentAverage.text = newValue.toString
+  }
