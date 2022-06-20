f9f8e8d2743d6b4ce2216c9168f34634500a9d42
objektwerks <objektwerks@runbox.com>
2022-06-20 10:01:58 -0400

    added model change listener to total chlorine pane

--- src/main/scala/pool/pane/dashboard/TotalChlorinePane.scala
+++ src/main/scala/pool/pane/dashboard/TotalChlorinePane.scala
@@ -4,3 +4,11 @@ import pool.Context
 
 class TotalChlorinePane(context: Context) extends DashboardTitledPane(context):
   text = context.tableTotalChlorine
+
+  context.model.currentTotalChlorine.onChange { (_, _, newValue) =>
+    currentValue.text = newValue.toString
+  }
+
+  context.model.averageTotalChlorine.onChange { (_, _, newValue) =>
+    currentAverage.text = newValue.toString
+  }
