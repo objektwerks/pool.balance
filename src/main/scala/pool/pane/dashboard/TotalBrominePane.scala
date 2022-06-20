08d4227aae67c2ae66fc639cee2c921d2c3d4055
objektwerks <objektwerks@runbox.com>
2022-06-20 10:10:05 -0400

    added model change listener to total bromine pane

--- src/main/scala/pool/pane/dashboard/TotalBrominePane.scala
+++ src/main/scala/pool/pane/dashboard/TotalBrominePane.scala
@@ -4,3 +4,11 @@ import pool.Context
 
 class TotalBrominePane(context: Context) extends DashboardTitledPane(context):
   text = context.tableTotalBromine
+
+  context.model.currentTotalBromine.onChange { (_, _, newValue) =>
+    currentValue.text = newValue.toString
+  }
+
+  context.model.averageTotalBromine.onChange { (_, _, newValue) =>
+    currentAverage.text = newValue.toString
+  }
