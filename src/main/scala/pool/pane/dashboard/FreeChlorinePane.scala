a382f7ea0f7873f4a53341280a3b32cebf8b132c
objektwerks <objektwerks@runbox.com>
2022-06-20 18:24:16 -0400

    replaced change listener with binding in free chlorine pane

--- src/main/scala/pool/pane/dashboard/FreeChlorinePane.scala
+++ src/main/scala/pool/pane/dashboard/FreeChlorinePane.scala
@@ -4,11 +4,5 @@ import pool.Context
 
 class FreeChlorinePane(context: Context) extends DashboardTitledPane(context):
   text = context.tableFreeChlorine
-
-  context.model.currentFreeChlorine.onChange { (_, _, newValue) =>
-    currentValue.text = newValue.toString
-  }
-
-  context.model.averageFreeChlorine.onChange { (_, _, newValue) =>
-    currentAverage.text = newValue.toString
-  }
+  currentValue.text <== context.model.currentFreeChlorine.asString
+  currentAverage.text <== context.model.averageFreeChlorine.asString
