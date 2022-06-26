5064cdc265f7ceb5d363093660bcac86833b76fa
objektwerks <objektwerks@runbox.com>
2022-06-26 07:53:00 -0400

    building out measurements chart

--- src/main/scala/pool/chart/MeasurementsChart.scala
+++ src/main/scala/pool/chart/MeasurementsChart.scala
@@ -1,8 +1,17 @@
 package pool.chart
 
+import java.time.format.DateTimeFormatter
+
 import scalafx.geometry.Insets
 import scalafx.scene.control.TabPane
 
-class MeasurementsChart extends TabPane with Chart:
+import pool.Context
+
+class MeasurementsChart(context: Context) extends TabPane with Chart:
+  val measurements = context.model.observableMeasurements
+  val dateFormatter = DateTimeFormatter.ofPattern("yy.D")
+  val minDate = measurements.map(m => m.measured).min.format(dateFormatter).toDouble
+  val maxDate = measurements.map(m => m.measured).max.format(dateFormatter).toDouble
+
   padding = Insets(6)
   tabs = List()
