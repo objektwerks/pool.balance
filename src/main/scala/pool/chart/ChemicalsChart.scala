7c0254a47a54e246718562f81d5301f88df18676
objektwerks <objektwerks@runbox.com>
2022-06-29 07:24:47 -0400

    added model to chemicals chart

--- src/main/scala/pool/chart/ChemicalsChart.scala
+++ src/main/scala/pool/chart/ChemicalsChart.scala
@@ -1,8 +1,19 @@
 package pool.chart
 
+import java.time.format.DateTimeFormatter
+
+import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
 import scalafx.geometry.Insets
 import scalafx.scene.control.TabPane
 
-class ChemicalsChart extends TabPane:
+import pool.Context
+
+class ChemicalsChart(context: Context) extends TabPane:
+  val chemicals = context.model.observableChemicals.reverse
+  val formatter = DateTimeFormatter.ofPattern("M.dd")
+  val minDate = chemicals.map(c => c.added).min.format(formatter).toDouble
+  val maxDate = chemicals.map(c => c.added).max.format(formatter).toDouble
+
   padding = Insets(6)
   tabs = List()
