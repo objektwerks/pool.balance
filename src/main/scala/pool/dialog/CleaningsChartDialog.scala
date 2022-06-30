ab23c6ce921889207bbf6b67df7de9a90bcfa896
objektwerks <objektwerks@runbox.com>
2022-06-30 07:17:31 -0400

    added cleanings chart dialog

---
+++ src/main/scala/pool/dialog/CleaningsChartDialog.scala
@@ -0,0 +1,20 @@
+package pool.dialog
+
+import scalafx.Includes.*
+import scalafx.scene.control.{ButtonType, Dialog}
+import scalafx.scene.control.ButtonBar.ButtonData
+import scalafx.scene.layout.VBox
+
+import pool.{App, Context}
+import pool.chart.CleaningsChart
+
+class CleaningsChartDialog(context: Context) extends Dialog[Unit]:
+  initOwner(App.stage)
+  title = context.windowTitle
+  headerText = context.chartChemicals
+  
+  dialogPane().buttonTypes = List(ButtonType.Close)
+  dialogPane().content = new VBox {
+    spacing = 6
+    children = List( CleaningsChart(context) )
+  }
