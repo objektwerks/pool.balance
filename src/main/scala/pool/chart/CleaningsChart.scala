a978436fcf0ae14f2b185cddf384a29a79cd9faf
objektwerks <objektwerks@runbox.com>
2022-06-30 07:20:58 -0400

    enhanced cleanings chart

--- src/main/scala/pool/chart/CleaningsChart.scala
+++ src/main/scala/pool/chart/CleaningsChart.scala
@@ -1,7 +1,13 @@
 package pool.chart
 
+import java.time.format.DateTimeFormatter
+
 import scalafx.scene.control.TabPane
 
 import pool.Context
 
-class CleaningsChart(context: Context) extends TabPane
+class CleaningsChart(context: Context) extends TabPane:
+  val cleanings = context.model.observableCleanings.reverse
+  val formatter = DateTimeFormatter.ofPattern("M.dd")
+  val minDate = cleanings.map(c => c.cleaned).min.format(formatter).toDouble
+  val maxDate = cleanings.map(c => c.cleaned).max.format(formatter).toDouble
