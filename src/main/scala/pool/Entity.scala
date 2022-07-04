f227d382613d85615432d536825f409eb4cdea7e
objektwerks <objektwerks@runbox.com>
2022-07-04 07:34:31 -0400

    simplified unit of measure conversion methods

--- src/main/scala/pool/Entity.scala
+++ src/main/scala/pool/Entity.scala
@@ -14,10 +14,10 @@ enum UnitOfMeasure:
 object UnitOfMeasure:
   def toList: List[String] = UnitOfMeasure.values.map(uom => uom.toString).toList
   def toPoolList: List[String] = List( UnitOfMeasure.gl.toString, UnitOfMeasure.l.toString )
-  def gallonsToLiters(gallons: Double): Double = gallons * 3.7854
-  def litersToGallons(liters: Double): Double = liters * 0.2641729
-  def poundsToKilograms(pounds: Double): Double = pounds * 0.45359237
-  def kilogramsToPounds(kilograms: Double): Double = kilograms * 2.20462
+  def gallonsToLiters(gallons: Double): Double = gallons * 3.785
+  def litersToGallons(liters: Double): Double = liters * 0.264
+  def poundsToKilograms(pounds: Double): Double = pounds * 0.454
+  def kilogramsToPounds(kilograms: Double): Double = kilograms * 2.205
 
 sealed trait Entity:
   val id: Long
