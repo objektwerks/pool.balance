816b21de153ddb5429b556ca8d29c9491c614989
objektwerks <objektwerks@runbox.com>
2022-07-04 07:36:30 -0400

    added unit of measure test

---
+++ src/test/scala/pool/UnitOfMeasureTest.scala
@@ -0,0 +1,13 @@
+package pool
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class UnitOfMeasureTest extends AnyFunSuite with Matchers:
+  test("unit of measure") {
+    UnitOfMeasure.valueOf(UnitOfMeasure.gl.toString) shouldBe UnitOfMeasure.gl
+    UnitOfMeasure.valueOf(UnitOfMeasure.l.toString) shouldBe UnitOfMeasure.l
+    UnitOfMeasure.valueOf(UnitOfMeasure.kg.toString) shouldBe UnitOfMeasure.kg
+    UnitOfMeasure.valueOf(UnitOfMeasure.lb.toString) shouldBe UnitOfMeasure.lb
+    UnitOfMeasure.valueOf(UnitOfMeasure.tablet.toString) shouldBe UnitOfMeasure.tablet
+  }
