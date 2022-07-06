8c9d8925d6e864a29fd5b0d67f9ad4338e5e9e0d
objektwerks <objektwerks@runbox.com>
2022-07-06 10:23:42 -0400

    enhanced errors pane

--- src/main/scala/pool/pane/ErrorsPane.scala
+++ src/main/scala/pool/pane/ErrorsPane.scala
@@ -9,7 +9,7 @@ import scalafx.scene.layout.VBox
 import pool.{Error, Context}
 
 class ErrorsPane(context: Context) extends VBox:
-  prefHeight = 50
+  prefHeight = 100
   spacing = 6
   padding = Insets(6)
 
