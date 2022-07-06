ffcf22e548d17b26cc131f9ab38621ff131a30e7
objektwerks <objektwerks@runbox.com>
2022-07-06 09:53:51 -0400

    enhanced errors pane

--- src/main/scala/pool/pane/ErrorsPane.scala
+++ src/main/scala/pool/pane/ErrorsPane.scala
@@ -1,9 +1,11 @@
 package pool.pane
 
 import scalafx.geometry.Insets
+import scalafx.scene.control.{TableColumn, TableView}
 import scalafx.scene.layout.VBox
 
 import pool.Context
+import scalafx.beans.value.ObservableValue
 
 class ErrorsPane(context: Context) extends VBox:
   spacing = 6
@@ -11,4 +13,19 @@ class ErrorsPane(context: Context) extends VBox:
 
   val model = context.model
 
-  children = List()
+  val tableView = new TableView[Error]() {
+    columns ++= List(
+      new TableColumn[Error, String] {
+        prefWidth = 150
+        text = context.headerAdded
+        cellValueFactory = _.value.
+      },
+      new TableColumn[Error, String] {
+        text = context.headerAdded
+        cellValueFactory = _.value.
+      }
+    )
+    items = model.observableErrors
+  }
+
+  children = List(tableView)
