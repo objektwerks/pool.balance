c60cf1992eaa35997424b01d8d457fe6b333ef3a
objektwerks <objektwerks@runbox.com>
2022-07-06 10:43:10 -0400

    converted errors pane into errors dialog

---
+++ src/main/scala/pool/dialog/ErrorsPane.scala
@@ -0,0 +1,35 @@
+package pool.pane
+
+import scalafx.Includes.*
+import scalafx.scene.control.{ButtonType, Dialog, TableColumn, TableView}
+import scalafx.scene.layout.VBox
+
+import pool.{App, Error, Context}
+
+class ErrorsPane(context: Context) extends Dialog[Unit]:
+  initOwner(App.stage)
+  title = context.windowTitle
+  headerText = context.labelErrors
+
+  val model = context.model
+
+  val tableView = new TableView[Error]() {
+    columns ++= List(
+      new TableColumn[Error, String] {
+        prefWidth = 150
+        text = context.headerOccurred
+        cellValueFactory = _.value.messageProperty
+      },
+      new TableColumn[Error, String] {
+        text = context.headerError
+        cellValueFactory = _.value.occurredProperty
+      }
+    )
+    items = model.observableErrors
+  }
+
+  dialogPane().buttonTypes = List(ButtonType.Close)
+  dialogPane().content = new VBox {
+    spacing = 6
+    children = List(tableView)
+  }
