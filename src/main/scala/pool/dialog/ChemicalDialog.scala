adca1667d13bcb2796569761a535815daa957bb0
objektwerks <objektwerks@runbox.com>
2022-07-08 02:58:50 -0400

    removed dead button code in chemical dialog

--- src/main/scala/pool/dialog/ChemicalDialog.scala
+++ src/main/scala/pool/dialog/ChemicalDialog.scala
@@ -44,7 +44,6 @@ class ChemicalDialog(context: Context, chemical: Chemical) extends Dialog[Chemic
 
   val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
   pane.buttonTypes = List(saveButtonType, ButtonType.Cancel)
-  val saveButton = pane.lookupButton(saveButtonType)
 
   resultConverter = dialogButton => {
     if dialogButton == saveButtonType then
