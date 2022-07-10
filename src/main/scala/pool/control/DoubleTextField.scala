af71f049d2755a01f506712c89748944fe8790f7
objektwerks <objektwerks@runbox.com>
2022-07-10 07:44:52 -0400

    edited comment

--- src/main/scala/pool/control/DoubleTextField.scala
+++ src/main/scala/pool/control/DoubleTextField.scala
@@ -11,7 +11,7 @@ class DoubleTextField extends TextField:
   val converter = DoubleStringConverter()
   val filter: (Change) => Change = { (change: Change) =>
     if change.text.matches(DoubleTextField.regex) then
-      change // only if change is a number
+      change // only if change is a double
     else
       change.setText("") // else make no change
       change
