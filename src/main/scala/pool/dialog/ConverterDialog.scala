745d7994124ba09f65e4e5c705a3d6f5aa150638
objektwerks <objektwerks@runbox.com>
2022-07-10 10:22:24 -0400

    enhanced converter dialog

--- src/main/scala/pool/dialog/ConverterDialog.scala
+++ src/main/scala/pool/dialog/ConverterDialog.scala
@@ -1,10 +1,20 @@
 package pool.dialog
 
-import scalafx.scene.control.Dialog
+import scalafx.Includes.*
+import scalafx.scene.control.{ButtonType, Dialog}
+import scalafx.scene.layout.VBox
 
 import pool.{App, Context}
 
-class ConverterDialog(context: Context) extends Dialog[Number]:
+class ConverterDialog(context: Context) extends Dialog[Unit]:
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.dialogConverters
+
+  dialogPane().buttonTypes = List(ButtonType.Close)
+  dialogPane().content = new VBox {
+    prefWidth = 600
+    prefHeight = 200
+    spacing = 6
+    children = List()
+  }
