ac792c9fec1a1753b620bafe5deeca0dade19ac0
objektwerks <objektwerks@runbox.com>
2022-07-10 10:20:03 -0400

    edited app and test.conf; context and converter dialog

--- src/main/scala/pool/dialog/ConverterDialog.scala
+++ src/main/scala/pool/dialog/ConverterDialog.scala
@@ -2,4 +2,9 @@ package pool.dialog
 
 import scalafx.scene.control.Dialog
 
-class ConverterDialog extends Dialog[Number]
+import pool.{App, Context}
+
+class ConverterDialog(context: Context) extends Dialog[Number]:
+  initOwner(App.stage)
+  title = context.windowTitle
+  headerText = context.dialogConverters
