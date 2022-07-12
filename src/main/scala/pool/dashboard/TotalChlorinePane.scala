eecfba6ea6ad0c4019e0bcdcff611accffb706c1
objektwerks <objektwerks@runbox.com>
2022-07-12 07:40:53 -0400

    edited app and test conf

--- src/main/scala/pool/dashboard/TotalChlorinePane.scala
+++ src/main/scala/pool/dashboard/TotalChlorinePane.scala
@@ -4,5 +4,7 @@ import pool.Context
 
 class TotalChlorinePane(context: Context) extends DashboardTitledPane(context):
   text = context.headerTotalChlorine
+  range.text = ""
+  ideal.text = ""
   current.text <== context.model.currentTotalChlorine.asString
   average.text <== context.model.averageTotalChlorine.asString
