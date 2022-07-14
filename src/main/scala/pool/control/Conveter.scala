601697ab5466f8af114703877096ef090e06ec46
objektwerks <objektwerks@runbox.com>
2022-07-14 07:36:11 -0400

    enhancec converter

--- src/main/scala/pool/control/Conveter.scala
+++ src/main/scala/pool/control/Conveter.scala
@@ -1,5 +1,25 @@
 package pool.control
 
+import scalafx.geometry.Insets
 import scalafx.scene.layout.GridPane
 
-class Conveter extends GridPane
+class Conveter extends GridPane:
+  hgap = 6
+  vgap = 6
+  padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
+
+  val glTextField = new DoubleTextField {
+    text = "0.0"
+  }
+
+  val lTextField = new DoubleTextField {
+    text = "0.0"
+  }
+
+  val lbTextField = new DoubleTextField {
+    text = "0.0"
+  }
+
+  val kgTextField = new DoubleTextField {
+    text = "0.0"
+  }
