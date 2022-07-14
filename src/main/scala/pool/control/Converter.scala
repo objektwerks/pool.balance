3630ccd1c4f43620007573f348b7ae8b0cd8c076
objektwerks <objektwerks@runbox.com>
2022-07-14 18:38:00 -0400

    refactored converter

--- src/main/scala/pool/control/Converter.scala
+++ src/main/scala/pool/control/Converter.scala
@@ -16,56 +16,72 @@ class Converter(context: Context) extends GridPane:
 
   val gallonsTextField = new DoubleTextField:
     text = "1.0"
-  
+  val gallonsTextFieldResult = new DoubleTextField:
+    text = ""
   val gallonsLabel = new Label:
     text = context.converterGallons
 
   val litersTextField = new DoubleTextField:
-    text = gallonsToLiters(1.0).toString
-
+    text = "1.0"
+  val litersTextFieldResult = new DoubleTextField:
+    text = ""
   val litersLabel = new Label:
     text = context.converterLiters
 
   val poundsTextField = new DoubleTextField:
     text = "1.0"
-
+  val poundsTextFieldResult = new DoubleTextField:
+    text = ""
   val poundsLabel = new Label:
     text = context.converterPounds
 
   val kilogramsTextField = new DoubleTextField:
-    text = poundsToKilograms(1.0).toString
-
+    text = "1.0"
+  val kilogramsTextFieldResult = new DoubleTextField:
+    text = ""
   val kilogramsLabel = new Label:
     text = context.converterKilograms
 
   gallonsTextField.text.onChange { (_, oldValue, newValue) =>
     println(s"gallons old: $oldValue new: $newValue")
-    if newValue.nonEmpty then litersTextField.text = gallonsToLiters( newValue.toDoubleOption.getOrElse(1.0) ).toString
+    litersTextFieldResult.text = gallonsToLiters( newValue.toDoubleOption.getOrElse(1.0) ).toString
   }
 
   litersTextField.text.onChange { (_, oldValue, newValue) =>
     println(s"liters old: $oldValue new: $newValue")
-    if newValue.nonEmpty then gallonsTextField.text = litersToGallons( newValue.toDoubleOption.getOrElse(1.0) ).toString
+    gallonsTextFieldResult.text = litersToGallons( newValue.toDoubleOption.getOrElse(1.0) ).toString
   }
 
   poundsTextField.text.onChange { (_, oldValue, newValue) =>
     println(s"pounds old: $oldValue new: $newValue")
-    if newValue.nonEmpty then kilogramsTextField.text = poundsToKilograms( newValue.toDoubleOption.getOrElse(1.0) ).toString
+    kilogramsTextFieldResult.text = poundsToKilograms( newValue.toDoubleOption.getOrElse(1.0) ).toString
   }
 
   kilogramsTextField.text.onChange { (_, oldValue, newValue) =>
     println(s"kilograms old: $oldValue new: $newValue")
-    if newValue.nonEmpty then poundsTextField.text = kilogramsToPounds( newValue.toDoubleOption.getOrElse(1.0) ).toString
+    poundsTextFieldResult.text = kilogramsToPounds( newValue.toDoubleOption.getOrElse(1.0) ).toString
   }
 
   add(gallonsTextField, columnIndex = 0, rowIndex = 0)
-  add(litersTextField, columnIndex = 1, rowIndex = 0)
+  add(litersTextFieldResult, columnIndex = 1, rowIndex = 0)
 
   add(gallonsLabel, columnIndex = 0, rowIndex = 1)
   add(litersLabel, columnIndex = 1, rowIndex = 1)
 
-  add(poundsTextField, columnIndex = 0, rowIndex = 2)
-  add(kilogramsTextField, columnIndex = 1, rowIndex = 2)
+  add(litersTextField, columnIndex = 0, rowIndex = 2)
+  add(gallonsTextFieldResult, columnIndex = 1, rowIndex = 2)
 
-  add(poundsLabel, columnIndex = 0, rowIndex = 3)
-  add(kilogramsLabel, columnIndex = 1, rowIndex = 3)
+  add(litersLabel, columnIndex = 0, rowIndex = 3)
+  add(gallonsLabel, columnIndex = 1, rowIndex = 3)
+
+  add(poundsTextField, columnIndex = 0, rowIndex = 4)
+  add(kilogramsTextFieldResult, columnIndex = 1, rowIndex = 4)
+
+  add(poundsLabel, columnIndex = 0, rowIndex = 5)
+  add(kilogramsLabel, columnIndex = 1, rowIndex = 5)
+
+  add(kilogramsTextField, columnIndex = 0, rowIndex = 6)
+  add(poundsTextFieldResult, columnIndex = 1, rowIndex = 6)
+
+  add(kilogramsLabel, columnIndex = 0, rowIndex = 7)
+  add(poundsLabel, columnIndex = 1, rowIndex = 7)
