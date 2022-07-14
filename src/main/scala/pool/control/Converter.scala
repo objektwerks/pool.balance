c0063fba912f5201ae0d2289b239e0605755a30d
objektwerks <objektwerks@runbox.com>
2022-07-14 14:46:31 -0400

    still refactoring converter

--- src/main/scala/pool/control/Converter.scala
+++ src/main/scala/pool/control/Converter.scala
@@ -1,7 +1,6 @@
 package pool.control
 
 import scalafx.Includes.*
-import scalafx.beans.property.ObjectProperty
 import scalafx.geometry.Insets
 import scalafx.scene.control.Label
 import scalafx.scene.layout.GridPane
@@ -15,53 +14,48 @@ class Converter(context: Context) extends GridPane:
   padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
   prefWidth = 200
 
-  val gallonsProperty = ObjectProperty[Double](0.0)
-  val litersProperty = ObjectProperty[Double](0.0)
-  val poundsProperty = ObjectProperty[Double](0.0)
-  val kilogramsProperty = ObjectProperty[Double](0.0)
-
   val gallonsTextField = new DoubleTextField:
-    text <== gallonsProperty.asString
-
+    text = "1.0"
+  
   val gallonsLabel = new Label:
     text = context.converterGallons
 
   val litersTextField = new DoubleTextField:
-    text <== litersProperty.asString
+    text = gallonsToLiters(1.0).toString
 
   val litersLabel = new Label:
     text = context.converterLiters
 
   val poundsTextField = new DoubleTextField:
-    text <== poundsProperty.asString
+    text = "1.0"
 
   val poundsLabel = new Label:
     text = context.converterPounds
 
   val kilogramsTextField = new DoubleTextField:
-    text <== kilogramsProperty.asString
+    text = poundsToKilograms(1.0).toString
 
   val kilogramsLabel = new Label:
     text = context.converterKilograms
 
   gallonsTextField.text.onChange { (_, oldValue, newValue) =>
     println(s"gallons old: $oldValue new: $newValue")
-    litersProperty.value = gallonsToLiters( newValue.toDoubleOption.getOrElse(1.0) )
+    litersTextField.text = gallonsToLiters( newValue.toDoubleOption.getOrElse(1.0) ).toString
   }
 
   litersTextField.text.onChange { (_, oldValue, newValue) =>
     println(s"liters old: $oldValue new: $newValue")
-    gallonsProperty.value = litersToGallons( newValue.toDoubleOption.getOrElse(1.0) )
+    gallonsTextField.text = litersToGallons( newValue.toDoubleOption.getOrElse(1.0) ).toString
   }
 
   poundsTextField.text.onChange { (_, oldValue, newValue) =>
     println(s"pounds old: $oldValue new: $newValue")
-    kilogramsProperty.value = poundsToKilograms( newValue.toDoubleOption.getOrElse(1.0) )
+    kilogramsTextField.text = poundsToKilograms( newValue.toDoubleOption.getOrElse(1.0) ).toString
   }
 
   kilogramsTextField.text.onChange { (_, oldValue, newValue) =>
     println(s"kilograms old: $oldValue new: $newValue")
-    poundsProperty.value = kilogramsToPounds( newValue.toDoubleOption.getOrElse(1.0) )
+    poundsTextField.text = kilogramsToPounds( newValue.toDoubleOption.getOrElse(1.0) ).toString
   }
 
   add(gallonsTextField, columnIndex = 0, rowIndex = 0)
