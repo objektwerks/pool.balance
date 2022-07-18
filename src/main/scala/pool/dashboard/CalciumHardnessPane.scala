3c0fcd7795c556d4012bb313d5b13c0dd31e975b
objektwerks <objektwerks@runbox.com>
2022-07-18 07:32:32 -0400

    added in range change listener to calcium hardness pane

--- src/main/scala/pool/dashboard/CalciumHardnessPane.scala
+++ src/main/scala/pool/dashboard/CalciumHardnessPane.scala
@@ -9,3 +9,7 @@ class CalciumHardnessPane(context: Context) extends DashboardTitledPane(context)
   ideal.text = context.dashboardCalciumHardnessIdeal
   current.text <== context.model.currentCalciumHardness.asString
   average.text <== context.model.averageCalciumHardness.asString
+
+  context.model.inRangeCalciumHardness.onChange { (_, _, inRange) =>
+    if inRange then println("calcium hardness in range") else println("calcium hardness out of range")
+  }
