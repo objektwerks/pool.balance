d11842837383c0d9eb9b03954cc68878a4a02c4a
objektwerks <objektwerks@runbox.com>
2022-07-18 07:35:41 -0400

    added in range change listener to salt pane

--- src/main/scala/pool/dashboard/SaltPane.scala
+++ src/main/scala/pool/dashboard/SaltPane.scala
@@ -9,3 +9,7 @@ class SaltPane(context: Context) extends DashboardTitledPane(context):
   ideal.text = context.dashboardSaltIdeal
   current.text <== context.model.currentSalt.asString
   average.text <== context.model.averageSalt.asString
+
+  context.model.inRangeSalt.onChange { (_, _, inRange) =>
+    if inRange then println("salt in range") else println("salt out of range")
+  }
