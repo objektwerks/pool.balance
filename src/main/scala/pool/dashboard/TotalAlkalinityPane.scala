03a67c5bfa813e270c062db02483ea9cac1b4915
objektwerks <objektwerks@runbox.com>
2022-07-18 07:36:34 -0400

    added in range change listener to total alkalinity pane

--- src/main/scala/pool/dashboard/TotalAlkalinityPane.scala
+++ src/main/scala/pool/dashboard/TotalAlkalinityPane.scala
@@ -9,3 +9,7 @@ class TotalAlkalinityPane(context: Context) extends DashboardTitledPane(context)
   ideal.text = context.dashboardTotalAlkalinityIdeal
   current.text <== context.model.currentTotalAlkalinity.asString
   average.text <== context.model.averageTotalAlkalinity.asString
+
+  context.model.inRangeTotalAlkalinity.onChange { (_, _, inRange) =>
+    if inRange then println("total alkalinity in range") else println("total alkalinity out of range")
+  }
