832ed65cfd11b12c61aacaf503074ca790d14700
objektwerks <objektwerks@runbox.com>
2022-07-21 15:03:28 -0400

    reformatted errors dialog

--- src/main/scala/pool/dialog/ErrorsDialog.scala
+++ src/main/scala/pool/dialog/ErrorsDialog.scala
@@ -6,32 +6,29 @@ import scalafx.scene.layout.VBox
 
 import pool.{App, Error, Context}
 
-class ErrorsDialog(context: Context) extends Dialog[Unit]:
+final class ErrorsDialog(context: Context) extends Dialog[Unit]:
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.dialogErrors
 
   val model = context.model
 
-  val tableView = new TableView[Error]() {
+  val tableView = new TableView[Error]():
     columns ++= List(
-      new TableColumn[Error, String] {
+      new TableColumn[Error, String]:
         text = context.headerOccurred
         cellValueFactory = _.value.occurredProperty
-      },
-      new TableColumn[Error, String] {
+      ,
+      new TableColumn[Error, String]:
         prefWidth = 425
         text = context.headerError
         cellValueFactory = _.value.messageProperty
-      }
     )
     items = model.observableErrors
-  }
 
   dialogPane().buttonTypes = List(ButtonType.Close)
-  dialogPane().content = new VBox {
+  dialogPane().content = new VBox:
     prefWidth = 600
     prefHeight = 200
     spacing = 6
     children = List(tableView)
-  }
