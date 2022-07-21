4c577bd00172e5d75326ddbcb0ba0a0962747539
objektwerks <objektwerks@runbox.com>
2022-07-21 15:00:41 -0400

    reformatted chemicals chart dialog

--- src/main/scala/pool/dialog/ChemicalsChartDialog.scala
+++ src/main/scala/pool/dialog/ChemicalsChartDialog.scala
@@ -8,13 +8,12 @@ import scalafx.scene.layout.VBox
 import pool.{App, Context}
 import pool.chart.ChemicalsChart
 
-class ChemicalsChartDialog(context: Context) extends Dialog[Unit]:
+final class ChemicalsChartDialog(context: Context) extends Dialog[Unit]:
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.chartChemicals
   
   dialogPane().buttonTypes = List(ButtonType.Close)
-  dialogPane().content = new VBox {
+  dialogPane().content = new VBox:
     spacing = 6
     children = List( ChemicalsChart(context) )
-  }
