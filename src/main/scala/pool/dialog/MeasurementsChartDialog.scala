8a3bd44cf88d247136e520c043b1fb07ef09fbb0
objektwerks <objektwerks@runbox.com>
2022-07-21 15:04:34 -0400

    reformatted measurements chart dialog

--- src/main/scala/pool/dialog/MeasurementsChartDialog.scala
+++ src/main/scala/pool/dialog/MeasurementsChartDialog.scala
@@ -8,13 +8,12 @@ import scalafx.scene.layout.VBox
 import pool.{App, Context}
 import pool.chart.MeasurementsChart
 
-class MeasurementsChartDialog(context: Context) extends Dialog[Unit]:
+final class MeasurementsChartDialog(context: Context) extends Dialog[Unit]:
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.chartMeasurements
   
   dialogPane().buttonTypes = List(ButtonType.Close)
-  dialogPane().content = new VBox {
+  dialogPane().content = new VBox:
     spacing = 6
     children = List( MeasurementsChart(context) )
-  }
