1c308146fc93199bbafa79d6711c48b896968bb0
objektwerks <objektwerks@runbox.com>
2022-07-21 15:02:37 -0400

    added final

--- src/main/scala/pool/dialog/ControlGridPane.scala
+++ src/main/scala/pool/dialog/ControlGridPane.scala
@@ -4,7 +4,7 @@ import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.control.Label
 import scalafx.scene.layout.{GridPane, Region}
 
-class ControlGridPane(controls: List[(String, Region)]) extends GridPane:
+final class ControlGridPane(controls: List[(String, Region)]) extends GridPane:
   hgap = 6
   vgap = 6
   padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
