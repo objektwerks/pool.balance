88ad7139dd60121bd21dce7ed75e375b459caf07
objektwerks <objektwerks@runbox.com>
2022-07-21 15:02:16 -0400

    reformatted cleanings chart dialog

--- src/main/scala/pool/dialog/CleaningsChartDialog.scala
+++ src/main/scala/pool/dialog/CleaningsChartDialog.scala
@@ -8,13 +8,12 @@ import scalafx.scene.layout.VBox
 import pool.{App, Context}
 import pool.chart.CleaningsChart
 
-class CleaningsChartDialog(context: Context) extends Dialog[Unit]:
+final class CleaningsChartDialog(context: Context) extends Dialog[Unit]:
   initOwner(App.stage)
   title = context.windowTitle
   headerText = context.chartCleanings
   
   dialogPane().buttonTypes = List(ButtonType.Close)
-  dialogPane().content = new VBox {
+  dialogPane().content = new VBox:
     spacing = 6
     children = List( CleaningsChart(context) )
-  }
