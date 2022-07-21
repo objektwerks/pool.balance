c73cd71602d357cf179bf99fcde38d7cc38a967b
objektwerks <objektwerks@runbox.com>
2022-07-21 14:51:33 -0400

    added final to int ext field

--- src/main/scala/pool/control/IntTextField.scala
+++ src/main/scala/pool/control/IntTextField.scala
@@ -7,7 +7,7 @@ import scalafx.util.converter.IntStringConverter
 object IntTextField:
   val regex = """[0-9]+""".r
 
-class IntTextField extends TextField:
+final class IntTextField extends TextField:
   val converter = IntStringConverter()
   val filter: Change => Change = { (change: Change) =>
     if IntTextField.regex.matches(change.text) then
