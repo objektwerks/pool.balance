c759c1422482363b4160fb0616bfdf16028f1cb8
objektwerks <objektwerks@runbox.com>
2022-07-21 12:29:26 -0400

    refactored free chlorine pane

--- src/main/scala/pool/dashboard/FreeChlorinePane.scala
+++ src/main/scala/pool/dashboard/FreeChlorinePane.scala
@@ -12,9 +12,9 @@ class FreeChlorinePane(context: Context) extends DashboardTitledPane(context):
   average.text <== context.model.averageFreeChlorine.asString
 
   context.model.currentFreeChlorine.onChange { (_, _, newValue) =>
-    if Measurement.freeChlorineRange.contains(newValue) then inRangeCurrent else outOfRangeCurrent
+    if context.model.freeChlorineInRange(newValue) then inRangeCurrent else outOfRangeCurrent
   }
 
   context.model.averageFreeChlorine.onChange { (_, _, newValue) =>
-    if Measurement.freeChlorineRange.contains(newValue) then inRangeAverage else outOfRangeAverage
+    if context.model.freeChlorineInRange(newValue) then inRangeAverage else outOfRangeAverage
   }
