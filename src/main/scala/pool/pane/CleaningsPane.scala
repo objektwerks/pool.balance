cfbf977756ab861f843bc9dacf7598c2c62a3f2d
objektwerks <objektwerks@runbox.com>
2022-07-21 14:37:34 -0400

    reformatted cleanings pane

--- src/main/scala/pool/pane/CleaningsPane.scala
+++ src/main/scala/pool/pane/CleaningsPane.scala
@@ -11,7 +11,7 @@ import scalafx.scene.layout.{HBox, Priority, VBox}
 import pool.{Cleaning, Context}
 import pool.dialog.{CleaningDialog, CleaningsChartDialog}
 
-class CleaningsPane(context: Context) extends VBox:
+final class CleaningsPane(context: Context) extends VBox:
   spacing = 6
   padding = Insets(6)
 
@@ -19,83 +19,77 @@ class CleaningsPane(context: Context) extends VBox:
 
   val yesOrNo = (bool: Boolean) => if bool then context.columnYes else context.columnNo
 
-  val tableView = new TableView[Cleaning]() {
+  val tableView = new TableView[Cleaning]():
     columns ++= List(
-      new TableColumn[Cleaning, Boolean] {
+      new TableColumn[Cleaning, Boolean]:
         text = context.headerBrush
         cellValueFactory = _.value.brushProperty
         cellFactory = (cell, bool) => {
           cell.text = yesOrNo(bool)
         }
-      },
-      new TableColumn[Cleaning, Boolean] {
+      ,
+      new TableColumn[Cleaning, Boolean]:
         text = context.headerNet
         cellValueFactory = _.value.netProperty
         cellFactory = (cell, bool) => {
           cell.text = yesOrNo(bool)
         }
-      },
-      new TableColumn[Cleaning, Boolean] {
+      ,
+      new TableColumn[Cleaning, Boolean]:
         text = context.headerSkimmerBasket
         cellValueFactory = _.value.skimmerBasketProperty
         cellFactory = (cell, bool) => {
           cell.text = yesOrNo(bool)
         }
-      },
-      new TableColumn[Cleaning, Boolean] {
+      ,
+      new TableColumn[Cleaning, Boolean]:
         text = context.headerPumpBasket
         cellValueFactory = _.value.pumpBasketProperty
         cellFactory = (cell, bool) => {
           cell.text = yesOrNo(bool)
         }
-      },
-      new TableColumn[Cleaning, Boolean] {
+      ,
+      new TableColumn[Cleaning, Boolean]:
         text = context.headerPumpFilter
         cellValueFactory = _.value.pumpFilterProperty
         cellFactory = (cell, bool) => {
           cell.text = yesOrNo(bool)
         }
-      },
-      new TableColumn[Cleaning, Boolean] {
+      ,
+      new TableColumn[Cleaning, Boolean]:
         text = context.headerVacuum
         cellValueFactory = _.value.vacuumProperty
         cellFactory = (cell, bool) => {
           cell.text = yesOrNo(bool)
         }
-      },
-      new TableColumn[Cleaning, String] {
+      ,
+      new TableColumn[Cleaning, String]:
         text = context.headerCleaned
         cellValueFactory = _.value.cleanedProperty
-      }
     )
     items = model.observableCleanings
-  }
 
-  val addButton = new Button {
+  val addButton = new Button:
     graphic = context.addImage
     text = context.buttonAdd
     disable = true
     onAction = { _ => add() }
-  }
 
-  val editButton = new Button {
+  val editButton = new Button:
     graphic = context.editImage
     text = context.buttonEdit
     disable = true
     onAction = { _ => update() }
-  }
 
-  val chartButton = new Button {
+  val chartButton = new Button:
     graphic = context.chartImage
     text = context.buttonChart
     disable = true
     onAction = { _ => chart() }
-  }
 
-  val buttonBar = new HBox {
+  val buttonBar = new HBox:
     spacing = 6
     children = List(addButton, editButton, chartButton)
-  }
 
   model.selectedPoolId.onChange { (_, _, _) =>
     addButton.disable = false
