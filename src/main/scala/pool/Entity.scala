c5c8f331b194283be0a512ca8e47ea7d23807cfa
objektwerks <objektwerks@runbox.com>
2022-07-21 07:59:16 -0400

    converted measurement ranges to inclusive

--- src/main/scala/pool/Entity.scala
+++ src/main/scala/pool/Entity.scala
@@ -79,16 +79,16 @@ final case class Cleaning(id: Long = 0,
   val cleaning = this
 
 object Measurement:
-  val totalChlorineRange = Range(1, 5)
-  val freeChlorineRange = Range(1, 5)
+  val totalChlorineRange = Range(1, 5).inclusive
+  val freeChlorineRange = Range(1, 5).inclusive
   val combinedChlorineRange = Set(0.0, 0.1, 0.2)
   val phRange = Set(7.2, 7.3, 7.4, 7.5, 7.6)
-  val calciumHardnessRange = Range(250, 500)
-  val totalAlkalinityRange = Range(80, 120)
-  val cyanuricAcidRange = Range(30, 100)
-  val totalBromineRange = Range(2, 10)
-  val saltRange = Range(2700, 3400)
-  val temperatureRange = Range(50, 100)
+  val calciumHardnessRange = Range(250, 500).inclusive
+  val totalAlkalinityRange = Range(80, 120).inclusive
+  val cyanuricAcidRange = Range(30, 100).inclusive
+  val totalBromineRange = Range(2, 10).inclusive
+  val saltRange = Range(2700, 3400).inclusive
+  val temperatureRange = Range(50, 100).inclusive
 
 final case class Measurement(id: Long = 0,
                              poolId: Long,
