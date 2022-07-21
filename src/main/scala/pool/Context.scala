3bf51985db37197dfa973d3d47282add6214587c
objektwerks <objektwerks@runbox.com>
2022-07-21 14:29:33 -0400

    reordered and reformatted context

--- src/main/scala/pool/Context.scala
+++ src/main/scala/pool/Context.scala
@@ -9,21 +9,6 @@ import scala.jdk.CollectionConverters.*
 import scalafx.scene.image.{Image, ImageView}
 
 final class Context(config: Config):
-  val url = config.getString("db.url")
-  val user = config.getString("db.user")
-  val password = config.getString("db.password")
-  val dataSourceClassName = config.getString("db.dataSourceClassName")
-  val maximumPoolSize = config.getInt("db.maximumPoolSize")
-  val dataSource: DataSource = {
-    val ds = new HikariDataSource()
-    ds.setDataSourceClassName(dataSourceClassName)
-    ds.addDataSourceProperty("url", url)
-    ds.addDataSourceProperty("user", user)
-    ds.addDataSourceProperty("password", password)
-    ds.setMaximumPoolSize(maximumPoolSize)
-    ds
-  }
-
   val windowTitle = config.getString("window.title")
   val windowWidth = config.getDouble("window.width")
   val windowHeight = config.getDouble("window.height")
@@ -180,13 +165,27 @@ final class Context(config: Config):
 
   def logo = new Image(Image.getClass.getResourceAsStream("/image/logo.png"))
 
+  val url = config.getString("db.url")
+  val user = config.getString("db.user")
+  val password = config.getString("db.password")
+  val dataSourceClassName = config.getString("db.dataSourceClassName")
+  val maximumPoolSize = config.getInt("db.maximumPoolSize")
+  val dataSource: DataSource = {
+    val ds = new HikariDataSource()
+    ds.setDataSourceClassName(dataSourceClassName)
+    ds.addDataSourceProperty("url", url)
+    ds.addDataSourceProperty("user", user)
+    ds.addDataSourceProperty("password", password)
+    ds.setMaximumPoolSize(maximumPoolSize)
+    ds
+  }
+
   val store = Store(this)
   val model = Model(this)
 
-  private def loadImageView(path: String): ImageView = new ImageView {
+  private def loadImageView(path: String): ImageView = new ImageView:
     image = new Image(Image.getClass.getResourceAsStream(path))
     fitHeight = 25
     fitWidth = 25
     preserveRatio = true
     smooth = true
-  }
