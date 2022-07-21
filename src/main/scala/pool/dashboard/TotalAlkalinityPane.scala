ca9e8af04e6794d8ec841592dbf7eff75c762214
objektwerks <objektwerks@runbox.com>
2022-07-21 04:27:21 -0400

    implemented total alkalinity pane in/out range current/average

--- src/main/scala/pool/dashboard/TotalAlkalinityPane.scala
+++ src/main/scala/pool/dashboard/TotalAlkalinityPane.scala
@@ -11,9 +11,9 @@ class TotalAlkalinityPane(context: Context) extends DashboardTitledPane(context)
   average.text <== context.model.averageTotalAlkalinity.asString
 
   context.model.rangeCurrentTotalAlkalinity.onChange { (_, _, inRange) =>
-    if inRange then println("current total alkalinity in range") else println("current total alkalinity out of range")
+    if inRange then inRangeCurrent else outOfRangeCurrent
   }
 
   context.model.rangeAverageTotalAlkalinity.onChange { (_, _, inRange) =>
-    if inRange then println("average total alkalinity in range") else println("average total alkalinity out of range")
+    if inRange then inRangeCurrent else outOfRangeAverage
   }
