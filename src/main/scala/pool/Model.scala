251a7daa4a0ba9e03eb5921305dad99172ddd693
objektwerks <objektwerks@runbox.com>
2022-07-21 11:57:56 -0400

    enhanced model

--- src/main/scala/pool/Model.scala
+++ src/main/scala/pool/Model.scala
@@ -228,3 +228,18 @@ final class Model(context: Context) extends LazyLogging:
       observableChemicals.sort()
       selectedChemicalId.value = chemical.id
     }
+
+    /*
+      val totalChlorineRange = Range(1, 5).inclusive
+      val freeChlorineRange = Range(1, 5).inclusive
+      val combinedChlorineRange = Set(0.0, 0.1, 0.2)
+      val phRange = Set(7.2, 7.3, 7.4, 7.5, 7.6)
+      val calciumHardnessRange = Range(250, 500).inclusive
+      val totalAlkalinityRange = Range(80, 120).inclusive
+      val cyanuricAcidRange = Range(30, 100).inclusive
+      val totalBromineRange = Range(2, 10).inclusive
+      val saltRange = Range(2700, 3400).inclusive
+      val temperatureRange = Range(50, 100).inclusive
+    */
+
+  def totalChlorineInRange(value: Int): Boolean = totalChlorineRange.contains(value)
