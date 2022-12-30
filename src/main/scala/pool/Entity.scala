c6ba3c8ac4e323c1d61a09bcd6a5ed571a37abf5
Objektwerks <objektwerks@runbox.com>
2022-12-30 04:53:23 -0500

    Update Entity.scala

--- src/main/scala/pool/Entity.scala
+++ src/main/scala/pool/Entity.scala
@@ -81,8 +81,8 @@ final case class Cleaning(id: Long = 0,
 object Measurement:
   val totalChlorineRange = Range(1, 5).inclusive
   val freeChlorineRange = Range(1, 5).inclusive
-  val combinedChlorineRange = Set(0.0, 0.1, 0.2)
-  val phRange = Set(7.2, 7.3, 7.4, 7.5, 7.6)
+  val combinedChlorineRange = Set(0.0, 0.1, 0.2, 0.3, 0.4, 0.5)
+  val phRange = Set(6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.1, 8.2, 8.3, 8.4)
   val calciumHardnessRange = Range(250, 500).inclusive
   val totalAlkalinityRange = Range(80, 120).inclusive
   val cyanuricAcidRange = Range(30, 100).inclusive
