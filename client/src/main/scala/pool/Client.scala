19e0228997a2425d7ec8073a2bcb3ffdeee9f4bf
objektwerks <objektwerks@runbox.com>
2023-01-10 14:55:58 -0500

    edited client

--- client/src/main/scala/pool/Client.scala
+++ client/src/main/scala/pool/Client.scala
@@ -10,7 +10,7 @@ import scalafx.scene.Scene
 import scalafx.scene.layout.VBox
 
 object Client extends JFXApp3 with LazyLogging:
-  private val context = Context( ConfigFactory.load("app.conf") )
+  private val context = Context( ConfigFactory.load("client.conf") )
 
   override def start(): Unit =    
     stage = new JFXApp3.PrimaryStage:
