c9895545b5e27814cc86f0113ca7987a3045c77d
objektwerks <objektwerks@runbox.com>
2023-01-12 09:15:25 -0500

    added command

---
+++ shared/src/main/scala/pool/Command.scala
@@ -0,0 +1,24 @@
+package pool
+
+sealed trait Command
+
+sealed trait License:
+  val license: String
+
+final case class Register(emailAddress: String) extends Command
+final case class Login(emailAddress: String, pin: String) extends Command
+
+final case class Deactivate(license: String) extends Command with License
+final case class Reactivate(license: String) extends Command with License
+
+final case class ListPools(license: String) extends Command with License
+final case class SavePool(license: String, pool: Pool) extends Command with License
+
+final case class ListCleanings(license: String) extends Command with License
+final case class SaveCleaning(license: String, cleaning: Cleaning) extends Command with License
+
+final case class ListMeasurements(license: String) extends Command with License
+final case class SaveMeasurement(license: String, measurement: Measurement) extends Command with License
+
+final case class ListChemicals(license: String) extends Command with License
+final case class SaveChemical(license: String, chemical: Chemical) extends Command with License
