a3de43048d6a3811fc6d093e57e3995e8f519ed4
objektwerks <objektwerks@runbox.com>
2023-01-12 13:51:30 -0500

    edited fetcher

--- client/src/main/scala/pool/Fetcher.scala
+++ client/src/main/scala/pool/Fetcher.scala
@@ -1,26 +1,4 @@
 package pool
 
-final class Fetcher:
-  def pools(): List[Pool] = ???
-
-  def add(pool: Pool): Pool = ???
-
-  def update(pool: Pool): Unit = ???
-
-  def cleanings(poolId: Long): List[Cleaning] = ???
-
-  def add(cleaning: Cleaning): Cleaning = ???
-
-  def update(cleaning: Cleaning): Unit = ???
-
-  def measurements(poolId: Long): List[Measurement] = ???
-
-  def add(measurement: Measurement): Measurement = ???
-
-  def update(measurement: Measurement): Unit = ???
-
-  def chemicals(poolId: Long): List[Chemical] = ???
-
-  def add(chemical: Chemical): Chemical = ???
-
-  def update(chemical: Chemical): Unit = ???
+final class Fetcher(url: String):
+  
