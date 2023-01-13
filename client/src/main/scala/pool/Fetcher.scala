03b90c7edb2b16e2e5c41e13476af64bd679b5e4
objektwerks <objektwerks@runbox.com>
2023-01-13 13:49:29 -0500

    enhanced fetcher

--- client/src/main/scala/pool/Fetcher.scala
+++ client/src/main/scala/pool/Fetcher.scala
@@ -1,7 +1,11 @@
 package pool
 
+import com.github.plokhotnyuk.jsoniter_scala.core.*
+
 import java.net.URI
 import java.net.http.HttpRequest
+
+import Serializer.given
 
 final class Fetcher(url: String):
   def call(command: Command,
