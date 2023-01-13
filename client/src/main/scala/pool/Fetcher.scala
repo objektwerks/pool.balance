6ea4dddebefcf5110332351f9dec2c40fdf82a24
objektwerks <objektwerks@runbox.com>
2023-01-13 13:48:48 -0500

    enhanced fetcher

--- client/src/main/scala/pool/Fetcher.scala
+++ client/src/main/scala/pool/Fetcher.scala
@@ -1,6 +1,10 @@
 package pool
 
+import java.net.URI
+import java.net.http.HttpRequest
+
 final class Fetcher(url: String):
   def call(command: Command,
-           handler: Event => Unit): Unit = ???
+           handler: Event => Unit): Unit =
+    val request = HttpRequest.newBuilder().uri(new URI(url))
   
