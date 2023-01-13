843a62be07e0cff82aac18de64d670417365d965
objektwerks <objektwerks@runbox.com>
2023-01-13 14:14:33 -0500

    refactored store test to adapt to refactored store

--- server/src/test/scala/pool/StoreTest.scala
+++ server/src/test/scala/pool/StoreTest.scala
@@ -32,64 +32,64 @@ final class StoreTest extends AnyFunSuite with Matchers:
 
   def addPool(): Pool =
     val pool = Pool(name = "pool-a", volume = 10000, unit = UnitOfMeasure.gl.toString)
-    val addedPool = store.add(pool)
-    addedPool.id should not be 0
-    addedPool
+    val id = store.addPool(pool)
+    id should not be 0
+    pool.copy(id = id)
 
   def updatePool(pool: Pool): Pool =
     val updatedPool = pool.copy(volume = 11000)
-    store.update(updatedPool)
+    store.updatePool(updatedPool)
     updatedPool
 
   def listPools(pool: Pool): Unit = 
-    val pools = store.pools()
+    val pools = store.listPools()
     pools.length shouldBe 1
     pools.head shouldBe pool
 
   def addCleaning(pool: Pool): Cleaning =
     val cleaning = Cleaning(poolId = pool.id, vacuum = true)
-    val addedCleaning = store.add(cleaning)
-    addedCleaning.id should not be 0
-    addedCleaning
+    val id = store.addCleaning(cleaning)
+    id should not be 0
+    cleaning.copy(id = id)
 
   def updateCleaning(cleaning: Cleaning): Cleaning =
     val updatedCleaning = cleaning.copy(net = true)
-    store.update(updatedCleaning)
+    store.updateCleaning(updatedCleaning)
     updatedCleaning
 
   def listCleanings(pool: Pool, cleaning: Cleaning): Unit =
-    val cleanings = store.cleanings(pool.id)
+    val cleanings = store.listCleanings(pool.id)
     cleanings.length shouldBe 1
     cleanings.head shouldBe cleaning
 
   def addMeasurement(pool: Pool): Measurement =
     val measurement = Measurement(poolId = pool.id)
-    val addedMeasurement = store.add(measurement)
-    addedMeasurement.id should not be 0
-    addedMeasurement
+    val id = store.addMeasurement(measurement)
+    id should not be 0
+    measurement.copy(id = id)
 
   def updateMeasurement(measurement: Measurement): Measurement =
     val updatedMeasurement = measurement.copy(ph = 7.3)
-    store.update(updatedMeasurement)
+    store.updateMeasurement(updatedMeasurement)
     updatedMeasurement
 
   def listMeasurements(pool: Pool, measurement: Measurement): Unit =
-    val measurements = store.measurements(pool.id)
+    val measurements = store.listMeasurements(pool.id)
     measurements.length shouldBe 1
     measurements.head shouldBe measurement
 
   def addChemical(pool: Pool): Chemical =
     val chemical = Chemical(poolId = pool.id, typeof = TypeOfChemical.Trichlor.toString, amount = 2.0, unit = UnitOfMeasure.gl.toString)
-    val addedChemcial = store.add(chemical)
-    addedChemcial.id should not be 0
-    addedChemcial
+    val id = store.addChemical(chemical)
+    id should not be 0
+    chemical.copy(id = id)
 
   def updateChemical(chemical: Chemical): Chemical =
     val updatedChemical = chemical.copy(amount = 2.5)
-    store.update(updatedChemical)
+    store.updateChemical(updatedChemical)
     updatedChemical
 
   def listChemicals(pool: Pool, chemical: Chemical): Unit =
-    val chemicals = store.chemicals(pool.id)
+    val chemicals = store.listChemicals(pool.id)
     chemicals.length shouldBe 1
     chemicals.head shouldBe chemical
