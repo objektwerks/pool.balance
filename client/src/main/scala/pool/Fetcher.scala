761b10214be671376aae06de5a3bbc813aba4db8
objektwerks <objektwerks@runbox.com>
2023-01-13 14:01:31 -0500

    enhanced fetcher

--- client/src/main/scala/pool/Fetcher.scala
+++ client/src/main/scala/pool/Fetcher.scala
@@ -4,6 +4,8 @@ import com.github.plokhotnyuk.jsoniter_scala.core.*
 
 import java.net.URI
 import java.net.http.{HttpClient, HttpRequest}
+import java.time.Duration
+import java.time.temporal.ChronoUnit.SECONDS
 
 import Serializer.given
 
@@ -14,8 +16,9 @@ final class Fetcher(url: String):
     val request = HttpRequest
       .newBuilder()
       .uri(URI(url))
+      .timeout(Duration.of(10, SECONDS))
       .version(HttpClient.Version.HTTP_2)
       .headers("Content-Type", "application/json; charset=UTF-8", "Accept", "application/json")
-      .POST(commandJson)
+      .POST( HttpRequest.BodyPublishers.ofString(commandJson) )
       .build()
   
