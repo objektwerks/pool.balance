fa184a4b18ff8f2493e073785aa0a7696a26fb6a
objektwerks <objektwerks@runbox.com>
2023-01-13 13:52:36 -0500

    edited fetcher

--- client/src/main/scala/pool/Fetcher.scala
+++ client/src/main/scala/pool/Fetcher.scala
@@ -3,12 +3,17 @@ package pool
 import com.github.plokhotnyuk.jsoniter_scala.core.*
 
 import java.net.URI
-import java.net.http.HttpRequest
+import java.net.http.{HttpClient, HttpRequest}
 
 import Serializer.given
 
 final class Fetcher(url: String):
   def call(command: Command,
            handler: Event => Unit): Unit =
-    val request = HttpRequest.newBuilder().uri(URI(url))
+    val request = HttpRequest
+      .newBuilder()
+      .uri(URI(url))
+      .version(HttpClient.Version.HTTP_2)
+      .POST()
+      .build()
   
