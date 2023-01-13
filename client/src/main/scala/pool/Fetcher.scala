aaa762af3fe5c36af02a03f989696b4187a7ffc4
objektwerks <objektwerks@runbox.com>
2023-01-13 13:55:54 -0500

    edited fetcher

--- client/src/main/scala/pool/Fetcher.scala
+++ client/src/main/scala/pool/Fetcher.scala
@@ -10,11 +10,12 @@ import Serializer.given
 final class Fetcher(url: String):
   def call(command: Command,
            handler: Event => Unit): Unit =
+    val commandJson = writeToString[Command](command)
     val request = HttpRequest
       .newBuilder()
       .uri(URI(url))
       .version(HttpClient.Version.HTTP_2)
       .headers("Content-Type", "application/json; charset=UTF-8", "Accept", "application/json")
-      .POST()
+      .POST(commandJson)
       .build()
   
