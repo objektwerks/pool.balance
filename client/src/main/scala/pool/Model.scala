bf34ee3dd82cd700237c4ddfd4fb6c49dcdf0cba
objektwerks <objektwerks@runbox.com>
2023-01-14 07:44:34 -0500

    removed model init method

--- client/src/main/scala/pool/Model.scala
+++ client/src/main/scala/pool/Model.scala
@@ -75,11 +75,6 @@ final class Model(fetcher: Fetcher) extends LazyLogging:
     logger.info(s"*** Model: observable chemicals onchange event: $changes")
   }
 
-  def init: Unit = // TODO Enable when server completed!!!
-    logger.info(s"*** Model: initializing ...")
-    pools()
-    logger.info(s"*** Model: initialized.")
-
   def onFault(cause: String): Unit =
     shouldBeInFxThread("onfault cause should be in fx thread.")
     observableFaults += Fault(cause)
