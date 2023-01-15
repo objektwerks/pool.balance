0a7716a5cf9db3e26989c8baf7a688407693cfb9
objektwerks <objektwerks@runbox.com>
2023-01-15 03:35:31 -0500

    added title to register login dialog

--- client/src/main/scala/pool/dialog/RegisterLoginDialog.scala
+++ client/src/main/scala/pool/dialog/RegisterLoginDialog.scala
@@ -3,9 +3,13 @@ package pool.dialog
 import scalafx.scene.control.Dialog
 import scalafx.stage.Stage
 
+import pool.Context
+
 final case class RegisterLogin(registerEmailAddress: Option[String],
                                loginEmailAddress: Option[String],
                                loginPin: Option[String])
 
-final class RegisterLoginDialog(owner: Stage) extends Dialog[RegisterLogin]:
+final class RegisterLoginDialog(owner: Stage, context: Context) extends Dialog[RegisterLogin]:
   initOwner(owner)
+  title = context.windowTitle
+  headerText = context.dialogPool
