84387124840550709b30255d6a819bce2183e6e4
objektwerks <objektwerks@runbox.com>
2023-01-15 09:10:53 -0500

    refactored imports

--- client/src/main/scala/pool/dashboard/PhPane.scala
+++ client/src/main/scala/pool/dashboard/PhPane.scala
@@ -1,7 +1,6 @@
 package pool.dashboard
 
-import pool.Context
-import pool.Measurement
+import pool.{Context, Measurement}
 
 final class PhPane(context: Context) extends DashboardTitledPane(context):
   text = context.headerPh
