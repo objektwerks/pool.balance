4c5602e7363d5c7ba4373cf51dc073a31257c204
objektwerks <objektwerks@runbox.com>
2023-01-15 09:06:58 -0500

    refactored imports

--- client/src/main/scala/pool/dialog/FaultsDialog.scala
+++ client/src/main/scala/pool/dialog/FaultsDialog.scala
@@ -4,8 +4,7 @@ import scalafx.Includes.*
 import scalafx.scene.control.{ButtonType, Dialog, TableColumn, TableView}
 import scalafx.scene.layout.VBox
 
-import pool.{Fault, Context}
-import pool.Client
+import pool.{Client, Context, Fault}
 
 final class FaultsDialog(context: Context) extends Dialog[Unit]:
   initOwner(Client.stage)
