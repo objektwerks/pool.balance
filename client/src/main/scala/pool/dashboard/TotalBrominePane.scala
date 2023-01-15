02a06953f4de165f43b3b8326bd38106d47e7d64
objektwerks <objektwerks@runbox.com>
2023-01-15 09:11:49 -0500

    refactored imports

--- client/src/main/scala/pool/dashboard/TotalBrominePane.scala
+++ client/src/main/scala/pool/dashboard/TotalBrominePane.scala
@@ -1,7 +1,6 @@
 package pool.dashboard
 
-import pool.Context
-import pool.Measurement
+import pool.{Context, Measurement}
 
 final class TotalBrominePane(context: Context) extends DashboardTitledPane(context):
   text = context.headerTotalBromine
