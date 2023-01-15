8ab69f1037ec5e2810104991f2547dc5690a9f9b
objektwerks <objektwerks@runbox.com>
2023-01-15 10:23:47 -0500

    enhanced client register login dialog match

--- client/src/main/scala/pool/Client.scala
+++ client/src/main/scala/pool/Client.scala
@@ -32,7 +32,9 @@ object Client extends JFXApp3 with LazyLogging:
     RegisterLoginDialog(stage, context).showAndWait() match
       case Some( RegisterLogin( Some(register), None) ) => model.register(register)
       case Some( RegisterLogin( None, Some(login) ) ) => model.login(login)
-      case _ => System.exit(-1)
+      case _ =>
+        logger.info("Client register or login failed!")
+        sys.exit(-1)
     
     logger.info(s"Client started targeting: $url")
 
