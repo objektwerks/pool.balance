230ec7d5b1ac3c736e6e415c9bce37eaa1ad5f13
objektwerks <objektwerks@runbox.com>
2023-01-15 09:11:06 -0500

    refactored imports

--- client/src/main/scala/pool/dashboard/SaltPane.scala
+++ client/src/main/scala/pool/dashboard/SaltPane.scala
@@ -1,7 +1,6 @@
 package pool.dashboard
 
-import pool.Context
-import pool.Measurement
+import pool.{Context, Measurement}
 
 final class SaltPane(context: Context) extends DashboardTitledPane(context):
   text = context.headerSalt
