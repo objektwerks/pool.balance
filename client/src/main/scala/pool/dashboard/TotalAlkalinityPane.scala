23d1ddfce23f07e67a98828b5226edc8fea9bba5
objektwerks <objektwerks@runbox.com>
2023-01-15 09:11:35 -0500

    refactored imports

--- client/src/main/scala/pool/dashboard/TotalAlkalinityPane.scala
+++ client/src/main/scala/pool/dashboard/TotalAlkalinityPane.scala
@@ -1,7 +1,6 @@
 package pool.dashboard
 
-import pool.Context
-import pool.Measurement
+import pool.{Context, Measurement}
 
 final class TotalAlkalinityPane(context: Context) extends DashboardTitledPane(context):
   text = context.headerTotalAlkalinity
