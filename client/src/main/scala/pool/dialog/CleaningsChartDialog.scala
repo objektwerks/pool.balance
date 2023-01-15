993ccaffd7113ffb4959fd93eb98da5a982583c4
objektwerks <objektwerks@runbox.com>
2023-01-15 09:07:24 -0500

    refactored imports

--- client/src/main/scala/pool/dialog/CleaningsChartDialog.scala
+++ client/src/main/scala/pool/dialog/CleaningsChartDialog.scala
@@ -5,8 +5,7 @@ import scalafx.scene.control.{ButtonType, Dialog}
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.layout.VBox
 
-import pool.{Context}
-import pool.Client
+import pool.{Client, Context}
 import pool.chart.CleaningsChart
 
 final class CleaningsChartDialog(context: Context) extends Dialog[Unit]:
