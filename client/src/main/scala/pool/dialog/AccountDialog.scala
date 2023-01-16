747c0d49a9defa7e77245dd6104cba30a0a746c7
objektwerks <objektwerks@runbox.com>
2023-01-16 08:24:53 -0500

    building out account dialog

--- client/src/main/scala/pool/dialog/AccountDialog.scala
+++ client/src/main/scala/pool/dialog/AccountDialog.scala
@@ -2,7 +2,9 @@ package pool.dialog
 
 import scalafx.scene.control.Dialog
 
-import pool.{Account, Context}
+import pool.{Account, Client, Context}
 
 final class AccountDialog(context: Context, account: Account) extends Dialog[Account]:
-  
+  initOwner(Client.stage)
+  title = context.windowTitle
+  headerText = context.dialogPool
