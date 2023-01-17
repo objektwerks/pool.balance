e186219ae903422ca9e596a097d91ee8d7eae22f
objektwerks <objektwerks@runbox.com>
2023-01-17 16:27:14 -0500

    edited fetcher

--- client/src/main/scala/pool/Fetcher.scala
+++ client/src/main/scala/pool/Fetcher.scala
@@ -16,6 +16,7 @@ final class Fetcher(url: String):
                          .newBuilder()
                          .executor( Executors.newVirtualThreadPerTaskExecutor() )
                          .build()
+
   def call(command: Command,
            handler: Event => Unit): Unit =
     val commandJson = writeToString[Command](command)
