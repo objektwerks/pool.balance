6af9b9e36f91742648c386599aeca6bee87f4ad3
objektwerks <objektwerks@runbox.com>
2023-01-19 18:48:00 -0500

    enhanced integration test

--- server/src/test/scala/pool/IntegrationTest.scala
+++ server/src/test/scala/pool/IntegrationTest.scala
@@ -56,25 +56,25 @@ class IntegrationTest extends AnyFunSuite with Matchers:
       case Registered(account) =>
         assert( account.isActivated )
         testAccount = account
-      case _ => fail("Invalid registered event.")
+      case fault => fail("Invalid registered event.")
     
   def login: Unit =
     val login = Login(testAccount.emailAddress, testAccount.pin)
     dispatcher.dispatch(login) match
       case LoggedIn(account) => account shouldBe testAccount
-      case _ => fail("Invalid loggedin event.")
+      case fault => fail("Invalid loggedin event.")
 
   def deactivate: Unit =
     val deactivate = Deactivate(testAccount.license)
     dispatcher.dispatch(deactivate) match
       case Deactivated(account) => assert( account.isDeactivated )
-      case _ => fail("Invalid deactivated event.")
+      case fault => fail("Invalid deactivated event.")
 
   def reactivate: Unit =
     val reactivate = Reactivate(testAccount.license)
     dispatcher.dispatch(reactivate) match
       case Reactivated(account) => assert( account.isActivated )
-      case _ => fail("Invalid reactivated event.")
+      case fault => fail("Invalid reactivated event.")
 
   def addPool: Unit =
     testPool = Pool(id = 0, name = "a", volume = 8000, unit = UnitOfMeasure.gl.toString)
@@ -86,14 +86,14 @@ class IntegrationTest extends AnyFunSuite with Matchers:
         testCleaning = testCleaning.copy(poolId = id)
         testMeasurement = testMeasurement.copy(poolId = id)
         testChemical = testChemical.copy(poolId = id)
-      case _ => fail("Invalid pool saved event.")
+      case fault => fail("Invalid pool saved event.")
 
   def updatePool: Unit =
     testPool = testPool.copy(volume = 10000)
     val savePool = SavePool(testAccount.license, testPool)
     dispatcher.dispatch(savePool) match
       case PoolSaved(id) => id shouldBe testPool.id
-      case _ => fail("Invalid pool saved event.")
+      case fault => fail("Invalid pool saved event.")
     
   def listPools: Unit =
     val listPools = ListPools(testAccount.license)
@@ -101,7 +101,7 @@ class IntegrationTest extends AnyFunSuite with Matchers:
       case PoolsListed(pools) =>
         pools.length shouldBe 1
         pools.head shouldBe testPool
-      case _ => fail("Invalid pools listed event.")
+      case fault => fail("Invalid pools listed event.")
 
   def addCleaning: Unit =
     val saveCleaning = SaveCleaning(testAccount.license, testCleaning)
@@ -109,7 +109,7 @@ class IntegrationTest extends AnyFunSuite with Matchers:
       case CleaningSaved(id) =>
         id should not be 0
         testCleaning = testCleaning.copy(id = id)
-      case _ => fail("Invalid cleaning saved event.")
+      case fault => fail("Invalid cleaning saved event.")
 
   def updateCleaning: Unit =
     testCleaning = testCleaning.copy(vacuum = true)
@@ -117,7 +117,7 @@ class IntegrationTest extends AnyFunSuite with Matchers:
     dispatcher.dispatch(saveCleaning) match
       case CleaningSaved(id) =>
         id shouldBe testCleaning.id
-      case _ => fail("Invalid cleaning saved event.")
+      case fault => fail("Invalid cleaning saved event.")
 
   def listCleanings: Unit =
     val listCleanings = ListCleanings(testAccount.license, testPool.id)
@@ -125,7 +125,7 @@ class IntegrationTest extends AnyFunSuite with Matchers:
       case CleaningsListed(cleanings) =>
         cleanings.length shouldBe 1
         cleanings.head shouldBe testCleaning
-      case _ => fail("Invalid cleanings listed event.")
+      case fault => fail("Invalid cleanings listed event.")
     
   def addMeasurement: Unit =
     val saveMeasurement = SaveMeasurement(testAccount.license, testMeasurement)
@@ -133,14 +133,14 @@ class IntegrationTest extends AnyFunSuite with Matchers:
       case MeasurementSaved(id) =>
         id should not be 0
         testMeasurement = testMeasurement.copy(id = id)
-      case _ => fail("Invalid measurement saved event.")
+      case fault => fail("Invalid measurement saved event.")
 
   def updateMeasurement: Unit =
     testMeasurement = testMeasurement.copy(temperature = 82)
     val saveMeasurement = SaveMeasurement(testAccount.license, testMeasurement)
     dispatcher.dispatch(saveMeasurement) match
       case MeasurementSaved(id) => id shouldBe testMeasurement.id
-      case _ => fail("Invalid measurement saved event.")
+      case fault => fail("Invalid measurement saved event.")
 
   def listMeasurements: Unit =
     val listMeasurements = ListMeasurements(testAccount.license, testPool.id)
@@ -148,7 +148,7 @@ class IntegrationTest extends AnyFunSuite with Matchers:
       case MeasurementsListed(measurements) =>
         measurements.length shouldBe 1
         measurements.head shouldBe testMeasurement
-      case _ => fail("Invalid measurements listed event.")
+      case fault => fail("Invalid measurements listed event.")
 
   def addChemical: Unit =
     val saveChemical = SaveChemical(testAccount.license, testChemical)
@@ -156,14 +156,14 @@ class IntegrationTest extends AnyFunSuite with Matchers:
       case ChemicalSaved(id) =>
         id should not be 0
         testChemical = testChemical.copy(id = id)
-      case _ => fail("Invalid chemical saved event.")    
+      case fault => fail("Invalid chemical saved event.")    
 
   def updateChemical: Unit =
     testChemical = testChemical.copy(amount = 2.0)
     val saveChemical = SaveChemical(testAccount.license, testChemical)
     dispatcher.dispatch(saveChemical) match
       case ChemicalSaved(id) => id shouldBe testChemical.id
-      case _ => fail("Invalid chemical saved event.")
+      case fault => fail("Invalid chemical saved event.")
 
   def listChemicals: Unit =
     val listChemicals = ListChemicals(testAccount.license, testPool.id)
@@ -171,4 +171,4 @@ class IntegrationTest extends AnyFunSuite with Matchers:
       case ChemicalsListed(chemicals) =>
         chemicals.length shouldBe 1
         chemicals.head shouldBe testChemical
-      case _ => fail("Invalid chemicals listed event.")
+      case fault => fail("Invalid chemicals listed event.")
