fa310751209d5a3f66a21c4aa2db6aab14ee20cd
objektwerks <objektwerks@runbox.com>
2023-01-19 06:28:48 -0500

    enhanced integration test

--- server/src/test/scala/pool/IntegrationTest.scala
+++ server/src/test/scala/pool/IntegrationTest.scala
@@ -3,5 +3,7 @@ package pool
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import scala.sys.process.Process
+
 class IntegrationTest extends AnyFunSuite with Matchers:
-  
+  val exitCode = Process("psql -d poolbalance -f ddl.sql").run().exitValue()
