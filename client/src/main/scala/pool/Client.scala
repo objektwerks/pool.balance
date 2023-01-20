b70bc07c2d54f9e82d9e5c11de33a6a949c615ca
objektwerks <objektwerks@runbox.com>
2023-01-20 10:03:25 -0500

    simplified client

--- client/src/main/scala/pool/Client.scala
+++ client/src/main/scala/pool/Client.scala
@@ -25,13 +25,13 @@ object Client extends JFXApp3 with LazyLogging:
       minHeight = context.windowHeight
       icons.add(context.logo)
 
-    model.registered.onChange { (_, _, newValue) =>
+    model.registered.onChange { (_, _, _) =>
       Alerts.showRegisterAlert(context, stage)
       logger.info("Register failed. Client stopping ...")
       sys.exit(-1)
     }
 
-    model.loggedin.onChange { (_, _, newValue) =>
+    model.loggedin.onChange { (_, _, _) =>
       Alerts.showLoginAlert(context, stage)
       logger.info("Login failed. Client stopping ...")
       sys.exit(-1)
