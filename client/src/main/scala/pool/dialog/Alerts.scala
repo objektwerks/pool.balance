5d70c43a7bab8d771adb68c72aa9d36b100c9e84
objektwerks <objektwerks@runbox.com>
2023-01-20 09:38:17 -0500

    refactored alerts

--- client/src/main/scala/pool/dialog/Alerts.scala
+++ client/src/main/scala/pool/dialog/Alerts.scala
@@ -1,29 +1,25 @@
 package pool.dialog
 
 import scalafx.application.Platform
-import scalafx.scene.control.Alert
+import scalafx.scene.control.{Alert, ButtonType}
 import scalafx.scene.control.Alert.AlertType
 import scalafx.stage.Stage
 
 import pool.Context
 
 object Alerts:
-  def showRegisterAlert(context: Context, stage: Stage): Unit =
-    Platform.runLater(
-      new Alert(AlertType.Error) {
-        initOwner(stage)
-        title = context.windowTitle
-        headerText = context.buttonRegister
-        contentText = context.errorRegister
-      }.showAndWait()
-    )
+  def showRegisterAlert(context: Context, stage: Stage): Option[ButtonType] =
+    new Alert(AlertType.Error) {
+      initOwner(stage)
+      title = context.windowTitle
+      headerText = context.buttonRegister
+      contentText = context.errorRegister
+    }.showAndWait()
 
-  def showLoginAlert(context: Context, stage: Stage): Unit =
-    Platform.runLater(
-      new Alert(AlertType.Error) {
-        initOwner(stage)
-        title = context.windowTitle
-        headerText = context.buttonLogin
-        contentText = context.errorLogin
-      }.showAndWait()
-    )
+  def showLoginAlert(context: Context, stage: Stage): Option[ButtonType] =
+    new Alert(AlertType.Error) {
+      initOwner(stage)
+      title = context.windowTitle
+      headerText = context.buttonLogin
+      contentText = context.errorLogin
+    }.showAndWait()
