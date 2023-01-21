c2f6889809a5a24a9e50a56592533be9f64abb7f
objektwerks <objektwerks@runbox.com>
2023-01-21 17:40:06 -0500

    edited client logging

--- client/src/main/scala/pool/Client.scala
+++ client/src/main/scala/pool/Client.scala
@@ -30,13 +30,13 @@ object Client extends JFXApp3 with LazyLogging:
 
     model.registered.onChange { (_, _, _) =>
       Alerts.showRegisterAlert(context, stage)
-      logger.info("Register failed. Client stopping ...")
+      logger.info("*** Register failed. Client stopping ...")
       sys.exit(-1)
     }
 
     model.loggedin.onChange { (_, _, _) =>
       Alerts.showLoginAlert(context, stage)
-      logger.info("Login failed. Client stopping ...")
+      logger.info("*** Login failed. Client stopping ...")
       sys.exit(-1)
     }
     
@@ -45,15 +45,15 @@ object Client extends JFXApp3 with LazyLogging:
         case Some( RegisterLogin( Some(register), None) ) => model.register(register)
         case Some( RegisterLogin( None, Some(login) ) ) => model.login(login)
         case _ =>
-          logger.info("Register or login failed. Client is stopping ...")
+          logger.info("*** Register or login failed. Client is stopping ...")
           sys.exit(-1)
     }.recover { case error: Exception =>
-      logger.error(s"Client failed to connect to server: $url with error: ${error.getMessage}")
+      logger.error(s"*** Client failed to connect to server: $url with error: ${error.getMessage}")
       Alerts.showServerAlert(context, stage)
       sys.exit(-1)
     }
     
     stage.show()
-    logger.info(s"Client started, targeting: $url")
+    logger.info(s"*** Client started, targeting: $url")
 
-  override def stopApp(): Unit = logger.info("Client stopped.")
+  override def stopApp(): Unit = logger.info("*** Client stopped.")
