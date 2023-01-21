edb0e3eec7bee648dd5d5f8a7a7dfd83c33dd0f9
objektwerks <objektwerks@runbox.com>
2023-01-21 18:10:02 -0500

    removed model from context

--- client/src/main/scala/pool/Context.scala
+++ client/src/main/scala/pool/Context.scala
@@ -7,7 +7,7 @@ import javax.sql.DataSource
 import scala.jdk.CollectionConverters.*
 import scalafx.scene.image.{Image, ImageView}
 
-final class Context(config: Config, val model: Model):
+final class Context(config: Config):
   val windowTitle = config.getString("window.title")
   val windowWidth = config.getDouble("window.width")
   val windowHeight = config.getDouble("window.height")
