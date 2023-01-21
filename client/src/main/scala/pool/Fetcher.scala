ddf9f2c87dfc6244eec9c8690858fe02385daf7e
objektwerks <objektwerks@runbox.com>
2023-01-21 16:13:33 -0500

    refactored fetcher

--- client/src/main/scala/pool/Fetcher.scala
+++ client/src/main/scala/pool/Fetcher.scala
@@ -3,7 +3,7 @@ package pool
 import com.github.plokhotnyuk.jsoniter_scala.core.*
 
 import java.net.URI
-import java.net.http.{HttpClient, HttpRequest}
+import java.net.http.{HttpClient, HttpRequest, HttpResponse}
 import java.net.http.HttpResponse.BodyHandlers
 import java.time.Duration
 import java.time.temporal.ChronoUnit.SECONDS
@@ -12,27 +12,34 @@ import java.util.concurrent.Executors
 import Serializer.given
 
 final class Fetcher(url: String):
+  private val uri = URI(url)
   private val client = HttpClient
                          .newBuilder()
                          .executor( Executors.newVirtualThreadPerTaskExecutor() )
                          .build()
 
-  private def toJson(command: Command): String = writeToString[Command](command)
-  private def fromJson(json: String): Event = readFromString[Event](json)
+  private def fromCommandToJson(command: Command): String = writeToString[Command](command)
+
+  private def fromJsonToEvent(json: String): Event = readFromString[Event](json)
+
+  private def buildHttpRequest(json: String): HttpRequest =
+    HttpRequest
+          .newBuilder
+          .uri(uri)
+          .timeout(Duration.of(10, SECONDS))
+          .version(HttpClient.Version.HTTP_2)
+          .headers("Content-Type", "application/json; charset=UTF-8", "Accept", "application/json")
+          .POST( HttpRequest.BodyPublishers.ofString(json) )
+          .build
+
+  private def sendHttpRequest(httpRequest: HttpRequest): HttpResponse[String] = client.send( httpRequest, BodyHandlers.ofString )
 
   def call(command: Command,
            handler: Event => Unit): Any =
-    val commandJson = toJson(command)
-    val request = HttpRequest
-      .newBuilder
-      .uri(URI(url))
-      .timeout(Duration.of(10, SECONDS))
-      .version(HttpClient.Version.HTTP_2)
-      .headers("Content-Type", "application/json; charset=UTF-8", "Accept", "application/json")
-      .POST( HttpRequest.BodyPublishers.ofString(commandJson) )
-      .build
-    
-    val response = client.send( request, BodyHandlers.ofString )
-    val event = fromJson( response.body )
+    val commandJson = fromCommandToJson(command)
+    val httpRequest = buildHttpRequest(commandJson)
+
+    val httpResponse = sendHttpRequest(httpRequest)
+    val event = fromJsonToEvent( httpResponse.body )
 
     handler(event)
