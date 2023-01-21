9bb625d662cf5eeb079078878f1324b0fd0e587e
objektwerks <objektwerks@runbox.com>
2023-01-21 17:44:46 -0500

    edited client logging

--- client/src/main/scala/pool/Client.scala
+++ client/src/main/scala/pool/Client.scala
@@ -30,13 +30,13 @@ object Client extends JFXApp3 with LazyLogging:
 
     model.registered.onChange { (_, _, _) =>
       Alerts.showRegisterAlert(context, stage)
-      logger.info("*** Register failed. Client stopping ...")
+      logger.error("*** Register failed. Client stopping ...")
       sys.exit(-1)
     }
 
     model.loggedin.onChange { (_, _, _) =>
       Alerts.showLoginAlert(context, stage)
-      logger.info("*** Login failed. Client stopping ...")
+      logger.error("*** Login failed. Client stopping ...")
       sys.exit(-1)
     }
     
@@ -45,7 +45,7 @@ object Client extends JFXApp3 with LazyLogging:
         case Some( RegisterLogin( Some(register), None) ) => model.register(register)
         case Some( RegisterLogin( None, Some(login) ) ) => model.login(login)
         case _ =>
-          logger.info("*** Register or login failed. Client is stopping ...")
+          logger.error("*** Register or login failed. Client is stopping ...")
           sys.exit(-1)
     }.recover { case error: Exception =>
       logger.error(s"*** Client failed to connect to server: $url with error: ${error.getMessage}")
