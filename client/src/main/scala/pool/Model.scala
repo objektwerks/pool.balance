4c07dbaaaf26bc8349ba73c9275e1a45e8939224
objektwerks <objektwerks@runbox.com>
2023-01-24 15:47:50 -0500

    enhanced model chemical save

--- client/src/main/scala/pool/Model.scala
+++ client/src/main/scala/pool/Model.scala
@@ -204,7 +204,8 @@ final class Model(fetcher: Fetcher) extends LazyLogging:
       (event: Event) => event match
         case fault @ Fault(_, _) => onFault("Model.save chemical", chemical, fault)
         case ChemicalSaved(id) =>
-          observableChemicals += chemical.copy(id = id)
+          if chemical.id == 0 then observableChemicals += chemical.copy(id = id)
+          else observableChemicals.update(observableChemicals.indexOf(chemical), chemical)
           observableChemicals.sort()
           selectedChemicalId.set(chemical.id)
         case _ => ()
