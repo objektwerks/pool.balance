83effeb431e141a3bd621f2fbb64557849ef39de
objektwerks <objektwerks@runbox.com>
2023-02-06 07:27:14 -0500

    added license to pool entity

--- shared/src/main/scala/pool/Entity.scala
+++ shared/src/main/scala/pool/Entity.scala
@@ -87,6 +87,7 @@ object Account:
   )
 
 final case class Pool(id: Long = 0,
+                      license: String = "",
                       name: String = "", 
                       volume: Int = 0,
                       unit: String = UnitOfMeasure.gl.toString) extends Entity:
