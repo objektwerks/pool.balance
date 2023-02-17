fceeedc00b061927332a950944a541441d916ce2
objektwerks <objektwerks@runbox.com>
2023-02-17 05:09:48 -0500

    edited dispatcher is authorized error message

--- server/src/main/scala/pool/Dispatcher.scala
+++ server/src/main/scala/pool/Dispatcher.scala
@@ -38,7 +38,7 @@ final class Dispatcher(store: Store,
         Try {
           store.isAuthorized(license.license)
         }.recover { case NonFatal(error) =>
-          logger.error(s"Is authorized failed: $error")
+          logger.error(s"Authorization failed: $error")
           false
         }.get
       case Register(_) | Login(_, _) => true
