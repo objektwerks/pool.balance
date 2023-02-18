2a58d396c7b79715417cb4f13a33dbd465e7dadb
objektwerks <objektwerks@runbox.com>
2023-02-18 08:07:26 -0500

    refactored dispatcher is authorized

--- server/src/main/scala/pool/Dispatcher.scala
+++ server/src/main/scala/pool/Dispatcher.scala
@@ -11,7 +11,11 @@ import Validator.*
 final class Dispatcher(store: Store, emailer: Emailer) extends LazyLogging:
   def dispatch[E <: Event](command: Command): Event =
     if !command.isValid then Fault(s"Command is invalid: $command")
-    if !isAuthorized(command) then Fault(s"License is unauthorized: $command")
+    isAuthorized(command) match
+      case Authorized(isAuthorized) => if !isAuthorized then Fault(s"License is unauthorized: $command")
+      case fault @ Fault(_, _) => fault
+      case _ =>
+        
     command match
       case Register(emailAddress)          => register(emailAddress)
       case Login(emailAddress, pin)        => login(emailAddress, pin)
@@ -26,18 +30,14 @@ final class Dispatcher(store: Store, emailer: Emailer) extends LazyLogging:
       case ListChemicals(_, poolId)        => listChemicals(poolId)
       case SaveChemical(_, chemical)       => saveChemical(chemical)
 
-  private def isAuthorized(command: Command): Boolean =
+  private def isAuthorized(command: Command): Event =
     command match
       case license: License =>
         Try {
-          store.isAuthorized(license.license)
-        }.recover { case NonFatal(error) =>
-          val message = s"Authorization failed: $error"
-          logger.error(message)
-          store.addFault( Fault(message) )
-          false
-        }.get
-      case Register(_) | Login(_, _) => true
+          Authorized( store.isAuthorized(license.license) )
+        }.recover { case NonFatal(error) => Fault(s"Authorization failed: $error") }
+         .get
+      case Register(_) | Login(_, _) => Authorized(true)
 
   private def register(emailAddress: String): Event =
     Try {
