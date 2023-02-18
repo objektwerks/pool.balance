da349ba50caecd0b56df1cfd43275dc7b14b5fa8
objektwerks <objektwerks@runbox.com>
2023-02-18 08:04:53 -0500

    added authorized event

--- shared/src/main/scala/pool/Event.scala
+++ shared/src/main/scala/pool/Event.scala
@@ -6,6 +6,8 @@ import scalafx.beans.property.ObjectProperty
 
 sealed trait Event
 
+final case class Authorized(isAuthorized: Boolean) extends Event
+
 final case class Registered(account: Account) extends Event
 final case class LoggedIn(account: Account) extends Event
 
