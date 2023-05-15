640030fd64e71f41cb043a802780ea4bb7bc52de
objektwerks <objektwerks@runbox.com>
2023-05-15 10:43:17 -0400

    removed import

--- client/src/main/scala/pool/dashboard/CalciumHardnessPane.scala
+++ client/src/main/scala/pool/dashboard/CalciumHardnessPane.scala
@@ -1,6 +1,6 @@
 package pool.dashboard
 
-import pool.{Context, Measurement, Model}
+import pool.{Context, Model}
 
 final class CalciumHardnessPane(context: Context, model: Model) extends DashboardTitledPane(context):
   text = context.headerCalciumHardness
