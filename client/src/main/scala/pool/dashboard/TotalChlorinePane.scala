9a54f2b8ddd6cc9b83043a8ff460423bf977203f
objektwerks <objektwerks@runbox.com>
2023-05-15 10:45:30 -0400

    removed import

--- client/src/main/scala/pool/dashboard/TotalChlorinePane.scala
+++ client/src/main/scala/pool/dashboard/TotalChlorinePane.scala
@@ -1,6 +1,6 @@
 package pool.dashboard
 
-import pool.{Context, Measurement, Model}
+import pool.{Context, Model}
 
 final class TotalChlorinePane(context: Context, model: Model) extends DashboardTitledPane(context):
   text = context.headerTotalChlorine
