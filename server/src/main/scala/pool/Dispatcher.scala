43b0a5df644e39011097d5ee5b5cf055717479bb
objektwerks <objektwerks@runbox.com>
2023-08-02 11:26:32 -0400

    enhanced dispatcher

--- server/src/main/scala/pool/Dispatcher.scala
+++ server/src/main/scala/pool/Dispatcher.scala
@@ -52,12 +52,10 @@ final class Dispatcher(store: Store, emailer: Emailer):
     }.recover { case NonFatal(error) => Fault(s"Registration failed for: $emailAddress, because: ${error.getMessage}") }
      .get
 
-  private val subject = "Account Registration"
-
   private def email(emailAddress: String, pin: String): Unit =
     val recipients = List(emailAddress)
     val message = s"<p>Save this pin: <b>${pin}</b> in a safe place; then delete this email.</p>"
-    emailer.send(recipients, subject, message)
+    emailer.send(recipients, message)
 
   private def login(emailAddress: String, pin: String): Event =
     Try { store.login(emailAddress, pin) }.fold(
