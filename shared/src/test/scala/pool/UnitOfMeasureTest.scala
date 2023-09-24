7bba7998d18730004ad3b5f54c5858ea216c7d00
Objektwerks <objektwerks@runbox.com>
2023-09-24 16:54:16 -0400

    Update UnitOfMeasureTest.scala

--- shared/src/test/scala/pool/UnitOfMeasureTest.scala
+++ shared/src/test/scala/pool/UnitOfMeasureTest.scala
@@ -5,8 +5,8 @@ import org.scalatest.matchers.should.Matchers
 
 import UnitOfMeasure.*
 
-class UnitOfMeasureTest extends AnyFunSuite with Matchers:
-  test("unit of measure") {
+final class UnitOfMeasureTest extends AnyFunSuite with Matchers:
+  test("unit of measure"):
     valueOf(gl.toString) shouldBe gl
     valueOf(l.toString) shouldBe l
     valueOf(kg.toString) shouldBe kg
@@ -14,23 +14,18 @@ class UnitOfMeasureTest extends AnyFunSuite with Matchers:
     valueOf(tablet.toString) shouldBe tablet
     toList.length shouldBe 5
     toPoolList.length shouldBe 2
-  }
 
-  test("gallons to liters") {
+  test("gallons to liters"):
     gallonsToLiters(1.0) shouldBe 3.785
     gallonsToLiters(2.0) shouldBe 7.57
- }
 
-  test("liters to gallons") {
+  test("liters to gallons"):
     litersToGallons(1.0) shouldBe 0.264
     litersToGallons(2.0) shouldBe 0.528
-  }
 
-  test("pounds to kilograms") {
+  test("pounds to kilograms"):
     poundsToKilograms(1.0) shouldBe 0.454
-  }
 
-  test("kilograms to pounds") {
+  test("kilograms to pounds"):
     kilogramsToPounds(1.0) shouldBe 2.205
     kilogramsToPounds(2.0) shouldBe 4.41
-  }
