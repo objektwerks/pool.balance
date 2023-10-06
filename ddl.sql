d102b2881d645db9b1b48c264898acd77fcaa242
objektwerks <objektwerks@runbox.com>
2023-10-06 14:25:39 -0400

    edited ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -3,16 +3,16 @@ CREATE SCHEMA PUBLIC;
 
 CREATE TABLE account (
   id BIGSERIAL PRIMARY KEY,
-  license VARCHAR(36) UNIQUE NOT NULL,
+  license CHAR(36) UNIQUE NOT NULL,
   email_address VARCHAR UNIQUE NOT NULL,
-  pin VARCHAR(7) NOT NULL,
+  pin CHAR(7) NOT NULL,
   activated BIGINT NOT NULL,
   deactivated BIGINT NOT NULL
 );
 
 CREATE TABLE pool (
   id BIGSERIAL PRIMARY KEY,
-  license VARCHAR(36) REFERENCES account(license),
+  license CHAR(36) REFERENCES account(license),
   name VARCHAR(24) NOT NULL,
   volume INT NOT NULL,
   unit VARCHAR NOT NULL
