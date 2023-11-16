53ccea8edd57240f6552f314e1076b29c0092b0b
objektwerks <objektwerks@runbox.com>
2023-11-16 13:13:15 -0500

    refactored handler

--- server/src/main/scala/pool/Handler.scala
+++ server/src/main/scala/pool/Handler.scala
@@ -1,35 +1,31 @@
 package pool
 
 import com.github.plokhotnyuk.jsoniter_scala.core.*
-import com.sun.net.httpserver.{HttpExchange, HttpHandler}
 import com.typesafe.scalalogging.Logger
 
-import scala.io.{Codec, Source}
+import io.helidon.webserver.http.{Handler => WebHandler, ServerRequest, ServerResponse}
 
 import Serializer.given
 
-object Handler:
-  def apply(dispatcher: Dispatcher,
-            store: Store,
-            logger: Logger): HttpHandler = new HttpHandler:
-    override def handle(exchange: HttpExchange): Unit =
-      val json = Source.fromInputStream( exchange.getRequestBody )(Codec.UTF8).mkString("")
-      val command = readFromString[Command](json)
-      logger.debug(s"*** Handler command: $command")
+final class Handler(dispatcher: Dispatcher,
+                    store: Store,
+                    logger: Logger) extends WebHandler:
+  override def handle(request: ServerRequest,
+                      response: ServerResponse): Unit =
+    val commandJson = request.content.as(classOf[String])
+    val command = readFromString[Command](commandJson)
+    logger.debug(s"*** Handler command: $command")
 
-      val event = dispatcher.dispatch(command)
-      logger.debug(s"*** Handler event: $event")
-      event match
-        case fault @ Fault(_, _) =>
-          logger.error(s"*** Handler fault: $fault")
-          store.addFault(fault)
-        case _ =>
-      val response = writeToString[Event](event)
+    val event = dispatcher.dispatch(command)
+    logger.debug(s"*** Handler event: $event")
+    event match
+      case fault @ Fault(_, _) =>
+        logger.error(s"*** Handler fault: $fault")
+        store.addFault(fault)
+      case _ =>
+    val eventJson = writeToString[Event](event)
 
-      exchange.sendResponseHeaders(200, response.length())
-      exchange.getResponseHeaders().add("Content-Type", "application/json; charset=UTF-8")
-
-      val outputStream = exchange.getResponseBody
-      outputStream.write(response.getBytes())
-      outputStream.flush()
-      outputStream.close()
+    response
+      .status(200)
+      .header("Content-Type", "application/json; charset=UTF-8")
+      .send(eventJson)
