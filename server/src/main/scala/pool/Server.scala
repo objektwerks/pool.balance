cf03204ea64c8ae9d8e77299be6d563ae82fea11
objektwerks <objektwerks@runbox.com>
2023-11-16 13:14:08 -0500

    refactored server

--- server/src/main/scala/pool/Server.scala
+++ server/src/main/scala/pool/Server.scala
@@ -1,37 +1,38 @@
 package pool
 
-import com.sun.net.httpserver.HttpServer
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
-import java.net.InetSocketAddress
-import java.util.concurrent.Executors
+import io.helidon.webserver.WebServer
+import io.helidon.webserver.http.HttpRouting
 
-import scala.concurrent.duration.*
+import scala.concurrent.duration.DurationInt
 
 object Server extends LazyLogging:
-  private val config = ConfigFactory.load("server.conf")
-  private val host = config.getString("host")
-  private val port = config.getInt("port")
-  private val backlog = 0
-
-  private val store = Store(config, Store.cache(minSize = 4, maxSize = 10, expireAfter = 24.hour))
-  private val emailer = Emailer(config)
-  private val dispatcher = Dispatcher(store, emailer)
-
-  private val http = HttpServer.create(InetSocketAddress(port), backlog)
-  private val handler = Handler(dispatcher, store, logger)
-
   @main def main(): Unit =
-    http.setExecutor(Executors.newVirtualThreadPerTaskExecutor())
-    http.createContext("/command", handler)
+    val config = ConfigFactory.load("server.conf")
+    val host = config.getString("server.host")
+    val port = config.getInt("server.port")
+    val endpoint = config.getString("server.endpoint")
 
-    http.start()
+    val store = Store(config, Store.cache(minSize = 4, maxSize = 10, expireAfter = 24.hour))
+    val emailer = Emailer(config)
+    val dispatcher = Dispatcher(store, emailer)
+
+    val handler = Handler(dispatcher, store, logger)
+
+    val builder = HttpRouting
+      .builder
+      .get(endpoint, handler)
+
+    WebServer
+      .builder
+      .port(port)
+      .routing(builder)
+      .build
+      .start
+
     println(s"*** Press Control-C to shutdown Pool Balance Http Server at: $host:$port")
     logger.info(s"*** Pool Balance Http Server started at: $host:$port")
-
-    sys.addShutdownHook:
-      http.stop(10)
-      logger.info(s"*** Pool Balance Http Server shutdown at: $host:$port")
 
     Thread.currentThread().join()
