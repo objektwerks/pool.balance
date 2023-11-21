e2e92dc59fa48bfc11574c233d986c2c37d33ebd
objektwerks <objektwerks@runbox.com>
2023-11-21 11:02:38 -0500

    optimized logging

--- client/src/main/scala/pool/Client.scala
+++ client/src/main/scala/pool/Client.scala
@@ -26,13 +26,13 @@ object Client extends JFXApp3 with LazyLogging:
 
     model.registered.onChange { (_, _, _) =>
       Alerts.showRegisterAlert(context, stage)
-      logger.error("*** Register failed. Client stopping ...")
+      logger.error("*** register failed, client stopping ...")
       sys.exit(-1)
     }
 
     model.loggedin.onChange { (_, _, _) =>
       Alerts.showLoginAlert(context, stage)
-      logger.error("*** Login failed. Client stopping ...")
+      logger.error("*** login failed, client stopping ...")
       sys.exit(-1)
     }
     
@@ -42,6 +42,6 @@ object Client extends JFXApp3 with LazyLogging:
       case _ =>
     
     stage.show()
-    logger.info(s"*** Client started, targeting: ${context.url}")
+    logger.info("*** client started, targeting: {}", context.url)
 
-  override def stopApp(): Unit = logger.info("*** Client stopped.")
+  override def stopApp(): Unit = logger.info("*** client stopped.")
