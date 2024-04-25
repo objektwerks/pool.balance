e28986d55d1f511892c4cd9905efce9e79b3d5d6
objektwerks <objektwerks@runbox.com>
2024-04-25 12:30:39 -0400

    edited serializer

--- shared/src/main/scala/pool/Serializer.scala
+++ shared/src/main/scala/pool/Serializer.scala
@@ -12,19 +12,19 @@ object Serializer:
   given JsonValueCodec[Chemical] = JsonCodecMaker.make[Chemical]
 
   given JsonValueCodec[Command] = JsonCodecMaker.make[Command]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[License] = JsonCodecMaker.make[License]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[Register] = JsonCodecMaker.make[Register]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[Login] = JsonCodecMaker.make[Login]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[Deactivate] = JsonCodecMaker.make[Deactivate]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[Reactivate] = JsonCodecMaker.make[Reactivate]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[ListPools] = JsonCodecMaker.make[ListPools]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[SavePool] = JsonCodecMaker.make[SavePool]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[ListCleanings] = JsonCodecMaker.make[ListCleanings]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[SaveCleaning] = JsonCodecMaker.make[SaveCleaning]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[ListMeasurements] = JsonCodecMaker.make[ListMeasurements]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[SaveMeasurement] = JsonCodecMaker.make[SaveMeasurement]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[ListChemicals] = JsonCodecMaker.make[ListChemicals]( CodecMakerConfig.withDiscriminatorFieldName(None) )
-  given JsonValueCodec[SaveChemical] = JsonCodecMaker.make[SaveChemical]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[License] = JsonCodecMaker.make[License]
+  given JsonValueCodec[Register] = JsonCodecMaker.make[Register]
+  given JsonValueCodec[Login] = JsonCodecMaker.make[Login]
+  given JsonValueCodec[Deactivate] = JsonCodecMaker.make[Deactivate]
+  given JsonValueCodec[Reactivate] = JsonCodecMaker.make[Reactivate]
+  given JsonValueCodec[ListPools] = JsonCodecMaker.make[ListPools]
+  given JsonValueCodec[SavePool] = JsonCodecMaker.make[SavePool]
+  given JsonValueCodec[ListCleanings] = JsonCodecMaker.make[ListCleanings]
+  given JsonValueCodec[SaveCleaning] = JsonCodecMaker.make[SaveCleaning]
+  given JsonValueCodec[ListMeasurements] = JsonCodecMaker.make[ListMeasurements]
+  given JsonValueCodec[SaveMeasurement] = JsonCodecMaker.make[SaveMeasurement]
+  given JsonValueCodec[ListChemicals] = JsonCodecMaker.make[ListChemicals]
+  given JsonValueCodec[SaveChemical] = JsonCodecMaker.make[SaveChemical]
 
   given JsonValueCodec[Event] = JsonCodecMaker.make[Event]( CodecMakerConfig.withDiscriminatorFieldName(None) )
   given JsonValueCodec[Registered] = JsonCodecMaker.make[Registered]( CodecMakerConfig.withDiscriminatorFieldName(None) )
