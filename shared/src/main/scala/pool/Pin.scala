e4d767e456cdb0729100a43a4e2091abc7f34713
Objektwerks <objektwerks@runbox.com>
2024-07-04 11:06:47 -0400

    Update Pin.scala

--- shared/src/main/scala/pool/Pin.scala
+++ shared/src/main/scala/pool/Pin.scala
@@ -4,16 +4,14 @@ import scala.util.Random
 
 object Pin:
   private val specialChars = "~!@#$%^&*-+=<>?/:;".toList
-  private val random = new Random
+  private val random = Random
 
-  private def newSpecialChar: Char = specialChars(random.nextInt(specialChars.length))
+  private def newSpecialChar: Char = specialChars( random.nextInt(specialChars.length) )
 
   def newInstance: String =
-    Random.shuffle(
-      Random
-        .alphanumeric
-        .take(5)
-        .mkString
-        .prepended(newSpecialChar)
-        .appended(newSpecialChar)
-    ).mkString
+    Random
+      .alphanumeric
+      .take(5)
+      .mkString
+      .prepended(newSpecialChar)
+      .appended(newSpecialChar)
