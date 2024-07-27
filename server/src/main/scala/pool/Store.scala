175aad4e7692fb79931b28b7bc0b723d86588a7b
objektwerks <objektwerks@runbox.com>
2024-07-27 15:49:00 -0400

    formatted store

--- server/src/main/scala/pool/Store.scala
+++ server/src/main/scala/pool/Store.scala
@@ -141,166 +141,180 @@ final class Store(cache: Cache[String, String],
       else None
     }
 
-  def listPools(license: String): List[Pool] = DB readOnly { implicit session =>
-    sql"select * from pool where license = $license order by name"
-      .map(rs =>
-        Pool(
-          rs.long("id"),
-          rs.string("license"),
-          rs.string("name"), 
-          rs.int("volume"), 
-          rs.string("unit")
+  def listPools(license: String): List[Pool] =
+    DB readOnly { implicit session =>
+      sql"select * from pool where license = $license order by name"
+        .map(rs =>
+          Pool(
+            rs.long("id"),
+            rs.string("license"),
+            rs.string("name"), 
+            rs.int("volume"), 
+            rs.string("unit")
+          )
         )
-      )
-      .list()
-  }
+        .list()
+    }
 
-  def addPool(pool: Pool): Long = DB localTx { implicit session =>
-    sql"""
-      insert into pool(name, license, volume, unit) values(${pool.name}, ${pool.license}, ${pool.volume}, ${pool.unit})
-      """
-      .updateAndReturnGeneratedKey()
-  }
+  def addPool(pool: Pool): Long =
+    DB localTx { implicit session =>
+      sql"""
+        insert into pool(name, license, volume, unit) values(${pool.name}, ${pool.license}, ${pool.volume}, ${pool.unit})
+        """
+        .updateAndReturnGeneratedKey()
+    }
 
-  def updatePool(pool: Pool): Long = DB localTx { implicit session =>
-    sql"""
-      update pool set name = ${pool.name}, volume = ${pool.volume}, unit = ${pool.unit}
-      where id = ${pool.id}
-      """
-      .update()
-    pool.id
-  }
+  def updatePool(pool: Pool): Long =
+    DB localTx { implicit session =>
+      sql"""
+        update pool set name = ${pool.name}, volume = ${pool.volume}, unit = ${pool.unit}
+        where id = ${pool.id}
+        """
+        .update()
+      pool.id
+    }
 
-  def listCleanings(poolId: Long): List[Cleaning] = DB readOnly { implicit session =>
-    sql"select * from cleaning where pool_id = $poolId order by cleaned desc"
-      .map(rs =>
-        Cleaning(
-          rs.long("id"),
-          rs.long("pool_id"),
-          rs.boolean("brush"),
-          rs.boolean("net"),
-          rs.boolean("skimmer_basket"),
-          rs.boolean("pump_basket"),
-          rs.boolean("pump_filter"),
-          rs.boolean("vacuum"),
-          rs.long("cleaned")
+  def listCleanings(poolId: Long): List[Cleaning] =
+    DB readOnly { implicit session =>
+      sql"select * from cleaning where pool_id = $poolId order by cleaned desc"
+        .map(rs =>
+          Cleaning(
+            rs.long("id"),
+            rs.long("pool_id"),
+            rs.boolean("brush"),
+            rs.boolean("net"),
+            rs.boolean("skimmer_basket"),
+            rs.boolean("pump_basket"),
+            rs.boolean("pump_filter"),
+            rs.boolean("vacuum"),
+            rs.long("cleaned")
+          )
         )
-      )
-      .list()
-  }
+        .list()
+    }
 
-  def addCleaning(cleaning: Cleaning): Long = DB localTx { implicit session =>
-    sql"""
-      insert into cleaning(pool_id, brush, net, skimmer_basket, pump_basket, pump_filter, vacuum, cleaned)
-      values(${cleaning.poolId}, ${cleaning.brush}, ${cleaning.net}, ${cleaning.skimmerBasket},
-      ${cleaning.pumpBasket}, ${cleaning.pumpFilter}, ${cleaning.vacuum}, ${cleaning.cleaned})
-      """
-      .updateAndReturnGeneratedKey()
-  }
+  def addCleaning(cleaning: Cleaning): Long =
+    DB localTx { implicit session =>
+      sql"""
+        insert into cleaning(pool_id, brush, net, skimmer_basket, pump_basket, pump_filter, vacuum, cleaned)
+        values(${cleaning.poolId}, ${cleaning.brush}, ${cleaning.net}, ${cleaning.skimmerBasket},
+        ${cleaning.pumpBasket}, ${cleaning.pumpFilter}, ${cleaning.vacuum}, ${cleaning.cleaned})
+        """
+        .updateAndReturnGeneratedKey()
+    }
 
-  def updateCleaning(cleaning: Cleaning): Long = DB localTx { implicit session =>
-    sql"""
-      update cleaning set brush = ${cleaning.brush}, net = ${cleaning.net}, skimmer_basket = ${cleaning.skimmerBasket},
-      pump_basket = ${cleaning.pumpBasket}, pump_filter = ${cleaning.pumpFilter}, vacuum = ${cleaning.vacuum},
-      cleaned = ${cleaning.cleaned} where id = ${cleaning.id}
-      """
-      .update()
-    cleaning.id
-  }
+  def updateCleaning(cleaning: Cleaning): Long =
+    DB localTx { implicit session =>
+      sql"""
+        update cleaning set brush = ${cleaning.brush}, net = ${cleaning.net}, skimmer_basket = ${cleaning.skimmerBasket},
+        pump_basket = ${cleaning.pumpBasket}, pump_filter = ${cleaning.pumpFilter}, vacuum = ${cleaning.vacuum},
+        cleaned = ${cleaning.cleaned} where id = ${cleaning.id}
+        """
+        .update()
+      cleaning.id
+    }
 
-  def listMeasurements(poolId: Long): List[Measurement] = DB readOnly { implicit session =>
-    sql"select * from measurement where pool_id = $poolId order by measured desc"
-      .map(rs =>
-        Measurement(
-          rs.long("id"),
-          rs.long("pool_id"),
-          rs.int("total_chlorine"),
-          rs.int("free_chlorine"),
-          rs.double("combined_chlorine"),
-          rs.double("ph"),
-          rs.int("calcium_hardness"),
-          rs.int("total_alkalinity"),
-          rs.int("cyanuric_acid"),
-          rs.int("total_bromine"),
-          rs.int("salt"),
-          rs.int("temperature"),
-          rs.long("measured")
+  def listMeasurements(poolId: Long): List[Measurement] =
+    DB readOnly { implicit session =>
+      sql"select * from measurement where pool_id = $poolId order by measured desc"
+        .map(rs =>
+          Measurement(
+            rs.long("id"),
+            rs.long("pool_id"),
+            rs.int("total_chlorine"),
+            rs.int("free_chlorine"),
+            rs.double("combined_chlorine"),
+            rs.double("ph"),
+            rs.int("calcium_hardness"),
+            rs.int("total_alkalinity"),
+            rs.int("cyanuric_acid"),
+            rs.int("total_bromine"),
+            rs.int("salt"),
+            rs.int("temperature"),
+            rs.long("measured")
+          )
         )
-      )
-      .list()
-  }
+        .list()
+    }
 
-  def addMeasurement(measurement: Measurement): Long = DB localTx { implicit session =>
-    sql"""
-      insert into measurement(pool_id, total_chlorine, free_chlorine, combined_chlorine, ph, calcium_hardness,
-      total_alkalinity, cyanuric_acid, total_bromine, salt, temperature, measured)
-      values(${measurement.poolId}, ${measurement.totalChlorine}, ${measurement.freeChlorine}, ${measurement.combinedChlorine},
-      ${measurement.ph}, ${measurement.calciumHardness}, ${measurement.totalAlkalinity}, ${measurement.cyanuricAcid},
-      ${measurement.totalBromine}, ${measurement.salt}, ${measurement.temperature}, ${measurement.measured})
-      """
-      .updateAndReturnGeneratedKey()
-  }
+  def addMeasurement(measurement: Measurement): Long =
+    DB localTx { implicit session =>
+      sql"""
+        insert into measurement(pool_id, total_chlorine, free_chlorine, combined_chlorine, ph, calcium_hardness,
+        total_alkalinity, cyanuric_acid, total_bromine, salt, temperature, measured)
+        values(${measurement.poolId}, ${measurement.totalChlorine}, ${measurement.freeChlorine}, ${measurement.combinedChlorine},
+        ${measurement.ph}, ${measurement.calciumHardness}, ${measurement.totalAlkalinity}, ${measurement.cyanuricAcid},
+        ${measurement.totalBromine}, ${measurement.salt}, ${measurement.temperature}, ${measurement.measured})
+        """
+        .updateAndReturnGeneratedKey()
+    }
 
-  def updateMeasurement(measurement: Measurement): Long = DB localTx { implicit session =>
-    sql"""
-      update measurement set total_chlorine = ${measurement.totalChlorine}, free_chlorine = ${measurement.freeChlorine},
-      combined_chlorine = ${measurement.combinedChlorine}, ph = ${measurement.ph}, calcium_hardness = ${measurement.calciumHardness},
-      total_alkalinity = ${measurement.totalAlkalinity}, cyanuric_acid = ${measurement.cyanuricAcid},
-      total_bromine = ${measurement.totalBromine}, salt = ${measurement.salt}, temperature = ${measurement.temperature},
-      measured = ${measurement.measured}
-      where id = ${measurement.id}
-      """
-      .update()
-    measurement.id
-  }
+  def updateMeasurement(measurement: Measurement): Long =
+    DB localTx { implicit session =>
+      sql"""
+        update measurement set total_chlorine = ${measurement.totalChlorine}, free_chlorine = ${measurement.freeChlorine},
+        combined_chlorine = ${measurement.combinedChlorine}, ph = ${measurement.ph}, calcium_hardness = ${measurement.calciumHardness},
+        total_alkalinity = ${measurement.totalAlkalinity}, cyanuric_acid = ${measurement.cyanuricAcid},
+        total_bromine = ${measurement.totalBromine}, salt = ${measurement.salt}, temperature = ${measurement.temperature},
+        measured = ${measurement.measured}
+        where id = ${measurement.id}
+        """
+        .update()
+      measurement.id
+    }
 
-  def listChemicals(poolId: Long): List[Chemical] = DB readOnly { implicit session =>
-    sql"select * from chemical where pool_id = $poolId order by added desc"
-      .map(rs =>
-        Chemical(
-          rs.long("id"),
-          rs.long("pool_id"),
-          rs.string("typeof"),
-          rs.double("amount"),
-          rs.string("unit"),
-          rs.long("added")
+  def listChemicals(poolId: Long): List[Chemical] =
+    DB readOnly { implicit session =>
+      sql"select * from chemical where pool_id = $poolId order by added desc"
+        .map(rs =>
+          Chemical(
+            rs.long("id"),
+            rs.long("pool_id"),
+            rs.string("typeof"),
+            rs.double("amount"),
+            rs.string("unit"),
+            rs.long("added")
+          )
         )
-      )
-      .list()
-  }
+        .list()
+    }
 
-  def addChemical(chemical: Chemical): Long = DB localTx { implicit session =>
-    sql"""
-      insert into chemical(pool_id, typeof, amount, unit, added)
-      values(${chemical.poolId}, ${chemical.typeof}, ${chemical.amount}, ${chemical.unit}, ${chemical.added})
-      """
-      .updateAndReturnGeneratedKey()
-  }
+  def addChemical(chemical: Chemical): Long =
+    DB localTx { implicit session =>
+      sql"""
+        insert into chemical(pool_id, typeof, amount, unit, added)
+        values(${chemical.poolId}, ${chemical.typeof}, ${chemical.amount}, ${chemical.unit}, ${chemical.added})
+        """
+        .updateAndReturnGeneratedKey()
+    }
 
-  def updateChemical(chemical: Chemical): Long = DB localTx { implicit session =>
-    sql"""
-      update chemical set typeof = ${chemical.typeof}, amount = ${chemical.amount}, unit = ${chemical.unit},
-      added = ${chemical.added} where id = ${chemical.id}
-      """
-      .update()
-    chemical.id
-  }
+  def updateChemical(chemical: Chemical): Long =
+    DB localTx { implicit session =>
+      sql"""
+        update chemical set typeof = ${chemical.typeof}, amount = ${chemical.amount}, unit = ${chemical.unit},
+        added = ${chemical.added} where id = ${chemical.id}
+        """
+        .update()
+      chemical.id
+    }
 
-  def listFaults(): List[Fault] = DB readOnly { implicit session =>
-    sql"select * from fault order by occurred desc"
-      .map(rs =>
-        Fault(
-          rs.string("cause"),
-          rs.string("occurred")
+  def listFaults(): List[Fault] =
+    DB readOnly { implicit session =>
+      sql"select * from fault order by occurred desc"
+        .map(rs =>
+          Fault(
+            rs.string("cause"),
+            rs.string("occurred")
+          )
         )
-      )
-      .list()
-  }
+        .list()
+    }
 
-  def addFault(fault: Fault): Fault = DB localTx { implicit session =>
-    sql"""
-      insert into fault(cause, occurred) values(${fault.cause}, ${fault.occurred})
-      """
-      .update()
-      fault
-  }
+  def addFault(fault: Fault): Fault =
+    DB localTx { implicit session =>
+      sql"""
+        insert into fault(cause, occurred) values(${fault.cause}, ${fault.occurred})
+        """
+        .update()
+        fault
+    }
