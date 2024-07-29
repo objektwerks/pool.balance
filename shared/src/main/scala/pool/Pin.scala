7af3b4848311d7ddb9321a5b95fa023fba01f758
objektwerks <objektwerks@runbox.com>
2024-07-29 08:01:41 -0400

    enhanced pin

--- shared/src/main/scala/pool/Pin.scala
+++ shared/src/main/scala/pool/Pin.scala
@@ -1,25 +1,30 @@
 package pool
 
+import java.util.concurrent.ThreadLocalRandom
+
 import scala.collection.mutable
 import scala.util.Random
 
 object Pin:
+  private val numbers = "0123456789"
   private val chars = "abcdefghijklmnopqrstuvwxyz"
   private val specialChars = "~!@#$%^&*-+=<>?/:;"
-  private val random = Random
+  private val shuffler = Random
 
-  private def newChar: Char = chars( random.nextInt(chars.length) )
-  private def newSpecialChar: Char = specialChars( random.nextInt(specialChars.length) )
+  private def newNumber(random: ThreadLocalRandom): Char = numbers( random.nextInt(numbers.length).toChar )
+  private def newChar(random: ThreadLocalRandom): Char = chars( random.nextInt(chars.length) )
+  private def newSpecialChar(random: ThreadLocalRandom): Char = specialChars( random.nextInt(specialChars.length) )
 
   def newInstance: String =
+    val random = ThreadLocalRandom.current()
     val pin = mutable.ArrayBuffer[Char]()
 
-    pin += newSpecialChar
-    pin += newChar.toUpper
-    pin += newChar.toLower
-    pin += newChar.toUpper
-    pin += newChar.toLower
-    pin += newChar.toUpper
-    pin += newSpecialChar
+    pin += newNumber(random)
+    pin += newChar(random).toUpper
+    pin += newChar(random).toLower
+    pin += newSpecialChar(random)
+    pin += newChar(random).toLower
+    pin += newChar(random).toUpper
+    pin += newSpecialChar(random)
 
-    pin.mkString
+    shuffler.shuffle(pin).mkString
