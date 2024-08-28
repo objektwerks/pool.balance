ea66f59eb648a2dde3e46ea9ee74b01e61e06cef
objektwerks <objektwerks@runbox.com>
2024-08-28 11:35:58 -0400

    refactored list cleanings

--- server/src/main/scala/pool/Dispatcher.scala
+++ server/src/main/scala/pool/Dispatcher.scala
@@ -116,11 +116,15 @@ final class Dispatcher(store: Store,
       case NonFatal(error) => Fault("Save pool failed:", error)
     .get
 
-  private def listCleanings(poolId: Long): Event =
-    Try {
-      CleaningsListed( store.listCleanings(poolId) )
-    }.recover { case NonFatal(error) => Fault("List cleanings failed:", error) }
-     .get
+  private def listCleanings(poolId: Long)(using IO): Event =
+    Try:
+      CleaningsListed(
+        supervised:
+          retry( RetryConfig.delay(1, 100.millis) )( store.listCleanings(poolId) )
+      )
+    .recover:
+      case NonFatal(error) => Fault("List cleanings failed:", error)
+    .get
 
   private def saveCleaning(cleaning: Cleaning): Event =
     Try {
