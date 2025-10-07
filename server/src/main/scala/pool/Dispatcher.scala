69a326b71cff2974744e703c58df2706590fee42
objektwerks <objektwerks@runbox.com>
2025-10-07 11:02:34 -0400

    edit

--- server/src/main/scala/pool/Dispatcher.scala
+++ server/src/main/scala/pool/Dispatcher.scala
@@ -157,7 +157,7 @@ final class Dispatcher(store: Store, emailer: Emailer):
     try
       ChemicalsListed(
         supervised:
-          retry( Schedule.fixedInterval(100.millis).maxRepeats(1) )( store.listChemicals(poolId) )
+          retry( Schedule.fixedInterval(100.millis).maxAttempts(1) )( store.listChemicals(poolId) )
       )
     catch
       case NonFatal(error) => Fault("List chemicals failed:", error)
